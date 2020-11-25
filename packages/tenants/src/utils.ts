30b75bfe80b0e787de1eaa885cf771538f84bc55
Mat Appelman <mat@opstrace.com>
2020-11-24 21:03:23 -0800

    fix: update to new setManagementOpion method on K8sResource
    Signed-off-by: Mat Appelman <mat@opstrace.com>

--- packages/tenants/src/utils.ts
+++ packages/tenants/src/utils.ts
@@ -45,7 +45,7 @@ export const serialize = (
     },
     kubeConfig
   );
-  cm.setOwnership({ protect: true }); // Protect so the reconciliation loop doesn't destroy it again.
+  cm.setManagementOption({ protect: true }); // Protect so the reconciliation loop doesn't destroy it again.
 
   return cm;
 };
