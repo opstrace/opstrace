589aa8279e0f1984c715336358595ac4d20fb7af
Mat Appelman <mat@opstrace.com>
2020-11-29 12:06:48 -0800

    fix: show 404 when navigating to unknown route
    Signed-off-by: Mat Appelman <mat@opstrace.com>

--- packages/app/src/client/views/404/404.tsx
+++ packages/app/src/client/views/404/404.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 import NotFoundComponent, {
   NotFoundProps
 } from "client/components/Error/NotFound";
 import { Page } from "client/components/Page";
 
+// Force this component to cover over any existing UI elements
+// by setting position to absolute and background to something opaque
+const StyledPage = styled(Page)`
+  background: ${props => props.theme.palette.background.default};
+`;
+
 const NotFound = (props: NotFoundProps) => {
   return (
-    <Page>
+    <StyledPage position="absolute">
       <NotFoundComponent {...props} />
-    </Page>
+    </StyledPage>
   );
 };
 
