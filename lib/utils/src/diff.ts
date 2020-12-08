183a1325a1a8c0ef7eb7d550b5948173631e83b7
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2020-12-08 19:10:43 +0100

    chore: add license header to utils/diff.ts
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- lib/utils/src/diff.ts
+++ lib/utils/src/diff.ts
@@ -1,7 +1,23 @@
-import DeepDiff from "deep-diff"
+/**
+ * Copyright 2020 Opstrace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import DeepDiff from "deep-diff";
 /**
  * Deep diff between two object, using lodash
  */
 export function diff(object: any, base: any) {
-	return DeepDiff.diff(object, base);
+  return DeepDiff.diff(object, base);
 }
