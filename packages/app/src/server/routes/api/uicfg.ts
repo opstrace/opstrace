c2038308014b101ef8a03a7d9f7681cc2efdb4a7
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2020-12-15 16:55:08 +0100

    ui: server: prepare a route for ui config
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

---
+++ packages/app/src/server/routes/api/uicfg.ts
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2020 Opstrace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import env from "server/env";
+import { NextFunction, Request, Response } from "express";
+
+export default function pubUiCfg(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.status(200).json({
+    auth0_client_id: env.AUTH0_CLIENT_ID,
+    auth0_domain: env.AUTH0_DOMAIN
+  });
+  return;
+}
