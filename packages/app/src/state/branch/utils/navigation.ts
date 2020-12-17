21c1e9dd821ee21f737031e03d79dca09e10ff78
Mat Appelman <mat@opstrace.com>
2020-12-17 14:04:23 -0800

    ui: alter navigation params for modules and branches
    Signed-off-by: Mat Appelman <mat@opstrace.com>

---
+++ packages/app/src/state/branch/utils/navigation.ts
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2020 Opstrace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { History } from "history";
+
+export default function navigateToBranch(branch: string, history: History) {
+  const parts = history.location.pathname
+    .replace(/^\//, "")
+    .split("/")
+    .filter(p => p.length);
+
+  const tab = (parts.length && parts.shift()) || "module";
+  // Pop existing branch off
+  if (parts.length) {
+    parts.shift();
+  }
+
+  history.push({
+    ...history.location,
+    pathname: `/${tab}/${branch}${parts.length ? `/${parts.join("/")}` : ""}`
+  });
+}
