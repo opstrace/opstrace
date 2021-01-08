a7a00d06ca340c83afcf21ca78a29108a24d7e63
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-01-08 11:16:11 +0100

    ci: gcp: no aaa yes bbb
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- ci/deploy-testremote-teardown.sh
+++ ci/deploy-testremote-teardown.sh
@@ -50,7 +50,7 @@ source secrets/aws-dev-svc-acc-env.sh
 # https://github.com/opstrace/opstrace/pull/128#issuecomment-742519078 and
 # https://stackoverflow.com/q/5189913/145400.
 #OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-aaa ci-shard-bbb ci-shard-ccc)
-OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-aaa ci-shard-ccc)
+OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-bbb ci-shard-ccc)
 echo "--- random choice for GCP project ID: ${OPSTRACE_GCP_PROJECT_ID}"
 export GOOGLE_APPLICATION_CREDENTIALS=./secrets/gcp-svc-acc-${OPSTRACE_GCP_PROJECT_ID}.json
 
