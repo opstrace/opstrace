52113eedce31d1c02e2e08e6f11c8c322e08c917
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-01-18 15:27:31 +0100

    ddapi: try to debug testcontainers-go tcp problem
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>
    
    ddapi: try to be explicit with DOCKER_HOST
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>
    
    ddapi: try SkipReaper
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>
    
    ddapi: outcomment makefile debug helpers
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>
    
    ddapi: handler test: fix bindmount confusion
    
    error was:
    
    Error response from daemon:
    invalid mount config for type "bind": bind source path does not exist:
    /build/go/pkg/ddapi/test-resources/cortex-dev-cfg.yaml
    
    Of course. Need to write the file onto the host of the
    Docker daemon.
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- go/pkg/ddapi/handler_test.go
+++ go/pkg/ddapi/handler_test.go
@@ -18,6 +18,8 @@ package ddapi
 import (
 	"context"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"testing"
@@ -45,17 +47,36 @@ func getPushURL(c testcontainers.Container, ctx context.Context) string {
 func TestSimpleInsert(t *testing.T) {
 	ctx := context.Background()
 
-	dir, err := os.Getwd()
+	// Assume that this is the directory that this package resides in.
+	// That's true at least when clicking `run test` in vs code :P
+	testdir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//  It is the caller's responsibility to remove the directory when no
+	//  longer needed.
+	tempdir, err := ioutil.TempDir("/tmp", "opstrace-go-unit-tests")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Copy that file into /tmp so that it can be shared across containers,
+	// and so that the bindMount source is valid _on the host_, too.
+	cortexCfgSourcePath := testdir + "/test-resources/cortex-dev-cfg.yaml"
+	cortexCfgHostPath := tempdir + "/cortex-dev-cfg.yaml"
+	simpleFileCopy(cortexCfgSourcePath, cortexCfgHostPath)
 
 	req := testcontainers.ContainerRequest{
 		Image:        "cortexproject/cortex:v1.6.0",
 		ExposedPorts: []string{"33333/tcp"},
 		WaitingFor:   wait.ForHTTP("/ready").WithPort("33333/tcp"),
-		BindMounts:   map[string]string{dir + "/test-resources/cortex-dev-cfg.yaml": "/cortex-config.yaml"},
+		BindMounts:   map[string]string{cortexCfgHostPath: "/cortex-config.yaml"},
 		Cmd:          []string{"exec", "./cortex", "-config.file=/cortex-config.yaml"},
+		// Attempt to work around
+		// creating reaper failed: could not start container: dial tcp: i/o timeout
+		// see #257
+		SkipReaper: true,
 	}
 
 	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
@@ -72,4 +93,26 @@ func TestSimpleInsert(t *testing.T) {
 	resp, _ := http.Get(pushURL)
 	// Expect 405, method not allowed
 	assert.Equal(t, resp.StatusCode, 405)
+}
+
+// Copy the src file to dst. Any existing file will be overwritten and will not
+// copy file attributes. https://stackoverflow.com/a/21061062/145400
+func simpleFileCopy(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
 }
