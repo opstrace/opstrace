ed188f6946aada0a547ba2f44de4896edc36c208
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-01-20 13:12:32 +0100

    ddapi: add unit test suite scaffold
    For suite setup and teardown,
    for managing resources:
    - temp dir
    - cortex container
    
    Works well, tested individual
    test execution in VSCode as well
    as via `make unit-tests` in go/
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- go/pkg/ddapi/handler_test.go
+++ go/pkg/ddapi/handler_test.go
@@ -21,30 +21,124 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func getPushURL(c testcontainers.Container, ctx context.Context) string {
-	ip, err := c.Host(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	port, err := c.MappedPort(ctx, "33333")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return fmt.Sprintf("http://%s:%s/api/v1/push", ip, port.Port())
+// Define the suite, and absorb the built-in basic suite
+// functionality from testify - including assertion methods.
+type Suite struct {
+	suite.Suite
+	tmpdir                 string
+	cortexContainerContext context.Context
+	cortexContainer        testcontainers.Container
+	cortexPushURL          string
 }
 
-func TestSimpleInsert(t *testing.T) {
+// `SetupSuite()`: is run once before all tests in the suite.
+func (suite *Suite) SetupSuite() {
+	d, err := createTmpDir()
+	if err != nil {
+		suite.T().Errorf("failed to create suite tmp dir: %v", err)
+		return
+	}
+	suite.tmpdir = d
+
+	ctx, cont, url, err := startCortex(suite.tmpdir)
+	if err != nil {
+		suite.T().Errorf("failed to launch cortex: %v", err)
+		return
+	}
+	suite.cortexContainerContext, suite.cortexContainer, suite.cortexPushURL = ctx, cont, url
+}
+
+// `TearDownSuite(): run once upon suite exit.
+// Note(JP): guarantees are a bit unclear. Also see
+// https://github.com/stretchr/testify/issues/764
+func (suite *Suite) TearDownSuite() {
+	// Destroy container (TODO: inspect state, fetch logs, etc)
+
+	log.Infof("suite teardown: attempt to destroy Cortex container")
+	err := suite.cortexContainer.Terminate(suite.cortexContainerContext)
+	if err != nil {
+		suite.T().Errorf("failed to destroy cortex container: %v", err)
+	}
+
+	// Note: leave this around for now for debugging?
+	log.Infof("suite teardown: attempt to remove tmp dir %s", suite.tmpdir)
+	err = os.RemoveAll(suite.tmpdir)
+	if err != nil {
+		suite.T().Errorf("failed to remove suite tmp dir: %v", err)
+	}
+}
+
+// Each suite method whose name starts with `Test` is run as a regular test.
+func (suite *Suite) TestCortexDirectly() {
+	// Test interacting straight with the containerized Cortex
+	// Expect 405 response: Method Not Allowed (only POST is supposed to work).
+	resp, err := http.Get(suite.cortexPushURL)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), resp.StatusCode, 405)
+}
+
+func (suite *Suite) TestDDCP() {
+	tenantName := "text"
+	remoteWriteURL := suite.cortexPushURL
+	disableAPIAuthentication := true
+	ddcp := NewDDCortexProxy(tenantName, remoteWriteURL, disableAPIAuthentication)
+
+	// resp, err := http.Get(suite.cortexPushURL)
+	// assert.NotNil(suite.T(), err)
+	// assert.Equal(suite.T(), resp.StatusCode, 405)
+
+	req := httptest.NewRequest("POST", "http://localhost/api/v1/series", nil)
+
+	checker := func(w *httptest.ResponseRecorder) {
+		resp := w.Result()
+
+		// Expect bad request, because there's no JSON body in the request
+		// And no content type header.
+		assert.Equal(suite.T(), resp.StatusCode, 400)
+
+		rbody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			suite.T().Errorf("got %v", err)
+		}
+
+		log.Infof("response body: %s", rbody)
+	}
+
+	w := httptest.NewRecorder()
+	ddcp.SeriesPostHandler(w, req)
+	checker(w)
+}
+
+// In order for 'go test' to run this suite, create a normal test function.
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(Suite))
+}
+
+func createTmpDir() (string, error) {
+	// Create a unique temporary directory for this test suite run. Note: "It
+	// is the caller's responsibility to remove the directory when no longer
+	// needed." -- do this in suite teardown.
+	tempdir, err := ioutil.TempDir("/tmp", "opstrace-go-unit-tests")
+	if err != nil {
+		return "", err
+	}
+
+	log.Infof("created suite tmp dir: %s", tempdir)
+	return tempdir, nil
+}
+
+func startCortex(tempdir string) (context.Context, testcontainers.Container, string, error) {
 	ctx := context.Background()
 
 	// Assume that this is the directory that this source file resides in.
@@ -53,14 +147,6 @@ func TestSimpleInsert(t *testing.T) {
 	// https://stackoverflow.com/a/23847429/145400
 	testdir, err := os.Getwd()
 	log.Infof("test dir: %s", testdir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// "It is the caller's responsibility to remove the directory when no
-	// longer needed.""
-	tempdir, err := ioutil.TempDir("/tmp", "opstrace-go-unit-tests")
-	log.Infof("created tmpdir: %s", tempdir)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -85,16 +171,23 @@ func TestSimpleInsert(t *testing.T) {
 		ContainerRequest: req,
 		Started:          true,
 	})
+
 	if err != nil {
-		t.Error(err)
+		return nil, nil, "", err
 	}
-	defer container.Terminate(ctx)
 
-	pushURL := getPushURL(container, ctx)
+	// Construct (base) URL to remote_write endpoint
+	ip, err := container.Host(ctx)
+	if err != nil {
+		panic(err)
+	}
+	port, err := container.MappedPort(ctx, "33333")
+	if err != nil {
+		panic(err)
+	}
+	pushURL := fmt.Sprintf("http://%s:%s/api/v1/push", ip, port.Port())
 
-	resp, _ := http.Get(pushURL)
-	// Expect 405, method not allowed
-	assert.Equal(t, resp.StatusCode, 405)
+	return ctx, container, pushURL, nil
 }
 
 // Copy the src file to dst. Any existing file will be overwritten and will not
