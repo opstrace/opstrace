5f78314ca07de01341640c026e3ea47655ada69d
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-01-21 14:10:32 +0100

    ddapi: testutil: DDTSFragment -> JSON via template
    A parameterization POC, can for example generate
    
    
    {
    	"series": [{
    		"metric": "unit.test.metric.TestSubmit<snip>Samples",
    		"points": [
    			[1611234498, 1],
    			[1611234499, 2],
    			[1611234500, 3],
    			[1611234501, 4],
    			[1611234502, 5],
    			[1611234503, 6],
    			[1611234504, 7],
    			[1611234505, 8],
    			[1611234506, 9],
    			[1611234507, 10]
    		],
    		"tags": [
    			"version:7.24.1"
    		],
    		"host": "x1carb6",
    		"type": "rate",
    		"interval": 10
    		}
    	]
    }
    
    
    from
    
    	tsfragment := &DDTSFragment{
    		metricname:           testname,
    		sampleCount:          10,
    		sampleValueIncrement: 1,
    	}
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- go/pkg/ddapi/testutil.go
+++ go/pkg/ddapi/testutil.go
@@ -24,44 +24,129 @@ import (
 	"text/template"
 	"time"
 
+	"github.com/lithammer/dedent"
 	log "github.com/sirupsen/logrus"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func createJSONBody(metricname string) string {
-	const jsonTextTemplate = `
+// A struct with parameters representing a DD time series fragment.
+// Most of these can be left "undefined" (zero) and will be filled
+// with simple default values, see `toJSON()` below.
+type DDTSFragment struct {
+	metricname           string
+	firstSampleTime      int64 // seconds since epoch
+	firstSampleValue     float64
+	sampleCount          int64
+	sampleValueIncrement float64
+	timestampIncrement   int64
+	keepValuesAsFloat    bool
+	metrictype           string
+}
+
+type datapoint struct {
+	TimestampString string
+	ValueString     string
+}
+
+// Using the string template approach here really allows us to control
+// everything about this JSON document, and it allows for quickly changing
+// properties without changing code. Useful for testing, iteration.
+func (tsfragment *DDTSFragment) toJSON() string {
+	jsonTextTemplate := `
 	{
-		"series": [
-		  {
+		"series": [{
 			"metric": "unit.test.metric.{{.metricname}}",
-			"points": [
-			  [
-				{{.secondsSinceEpoch}},
-				1
-			  ]
+			"points": [{{range $idx, $point := .points}}{{if $idx}},{{end}}
+				[{{$point.TimestampString}}, {{$point.ValueString}}]{{end}}
 			],
 			"tags": [
-			  "version:7.24.1"
+				"version:7.24.1"
 			],
 			"host": "x1carb6",
 			"type": "rate",
 			"interval": 10
-		  }
+			}
 		]
-	  }
+	}
 `
-	data := map[string]interface{}{
-		"metricname":        metricname,
-		"secondsSinceEpoch": fmt.Sprintf("%v", time.Now().Unix()),
+	// Remove common whitespace template lines, like textwrap.dedent() from
+	// Python's stdlib.
+	jsonTextTemplate = dedent.Dedent(jsonTextTemplate)
+
+	var firstSampleTime int64
+	var firstSampleValue float64
+	var sampleCount int64
+	var timestampIncrement int64
+	var metrictype string
+
+	// Poor man's default value implementation.
+	// Default to current time for first sample.
+	if tsfragment.firstSampleTime == 0 {
+		firstSampleTime = time.Now().Unix()
 	}
 
-	// Render template, store result in string.
+	log.Infof("tsfragment.sampleCount: %d", tsfragment.sampleCount)
+	sampleCount = tsfragment.sampleCount
+	if tsfragment.sampleCount == 0 {
+		sampleCount = 1
+	}
+	timestampIncrement = tsfragment.timestampIncrement
+	if tsfragment.timestampIncrement == 0 {
+		timestampIncrement = 1
+	}
+	firstSampleValue = tsfragment.firstSampleValue
+	if tsfragment.firstSampleValue == 0 {
+		firstSampleValue = 1.0
+	}
+	metrictype = tsfragment.metrictype
+	if tsfragment.metrictype == "" {
+		metrictype = "rate"
+	}
 
+	// keepValuesAsFloat defaults to false, i.e. convert to int. Huh, dirty.
+
+	// Generate string value For Template (FT)
+	// firstSampleTimeFT := fmt.Sprintf("%v", firstSampleTime)
+
+	// Generate data points, in particular their string representation for
+	// JSON construction.
+	points := make([]datapoint, sampleCount)
+	var i int64
+	for i = 0; i < sampleCount; i++ {
+		// Generate value string. The logic here makes it explicit that we
+		// distinguish two cases: in the resulting JSON doc -- should the
+		// number appear as integer or as float?
+		var valueString string
+		if tsfragment.keepValuesAsFloat {
+			valueString = fmt.Sprintf(
+				"%.5f",
+				firstSampleValue+float64(i)*tsfragment.sampleValueIncrement,
+			)
+		} else {
+			valueString = fmt.Sprintf(
+				"%d",
+				int64(firstSampleValue)+i*int64(tsfragment.sampleValueIncrement),
+			)
+		}
+
+		dp := datapoint{
+			TimestampString: fmt.Sprintf("%v", firstSampleTime+i*timestampIncrement),
+			ValueString:     valueString,
+		}
+		points[i] = dp
+	}
+
+	templatedata := map[string]interface{}{
+		"metricname": tsfragment.metricname,
+		"metrictype": metrictype,
+		"points":     points,
+	}
+
+	// Render template, write to buffer.
 	t := template.Must(template.New("").Parse(jsonTextTemplate))
-
 	buf := &bytes.Buffer{}
-	if err := t.Execute(buf, data); err != nil {
+	if err := t.Execute(buf, templatedata); err != nil {
 		panic(err)
 	}
 
