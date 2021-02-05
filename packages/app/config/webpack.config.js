ef649761f87db09c7b7b95d123a3c4888b1951ab
Mat Appelman <mat@opstrace.com>
2021-02-05 11:30:42 -0800

    ui: resolve the esm version of @material-ui for OpScript runtime
    Signed-off-by: Mat Appelman <mat@opstrace.com>

--- packages/app/config/webpack.config.js
+++ packages/app/config/webpack.config.js
@@ -385,6 +385,31 @@ module.exports = function (webpackEnv) {
                   "@babel/plugin-proposal-optional-chaining",
                   "@babel/plugin-proposal-class-properties",
                   "@babel/plugin-syntax-dynamic-import",
+                  [
+                    require.resolve('babel-plugin-transform-imports'),
+                    {
+                      '@material-ui/core': {
+                        // Use "transform: '@material-ui/core/${member}'," if your bundler does not support ES modules
+                        'transform': '@material-ui/core/esm/${member}',
+                        'preventFullImport': true
+                      },
+                      '@material-ui/icons': {
+                        // Use "transform: '@material-ui/icons/${member}'," if your bundler does not support ES modules
+                        'transform': '@material-ui/icons/esm/${member}',
+                        'preventFullImport': true
+                      },
+                      '@material-ui/styles': {
+                        // Use "transform: '@material-ui/icons/${member}'," if your bundler does not support ES modules
+                        'transform': '@material-ui/styles/esm/${member}',
+                        'preventFullImport': true
+                      },
+                      '@material-ui/lab': {
+                        // Use "transform: '@material-ui/icons/${member}'," if your bundler does not support ES modules
+                        'transform': '@material-ui/lab/esm/${member}',
+                        'preventFullImport': true
+                      }
+                    }
+                  ],
                   [require.resolve("@loadable/babel-plugin")],
                   [require.resolve("babel-plugin-styled-components")],
                   [
