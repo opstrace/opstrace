82e71d69d37f4b1e836426a3b028b2a0d9a22df1
Simão Reis <sreis@opstrace.com>
2021-02-09 12:39:57 -0100

    cli: introduce upgrade package
    Signed-off-by: Simão Reis <sreis@opstrace.com>

---
+++ packages/upgrader/src/index.ts
@@ -0,0 +1,206 @@
+/**
+ * Copyright 2020 Opstrace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  fork,
+  call,
+  race,
+  delay,
+  cancel
+} from "redux-saga/effects";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import { KubeConfig } from "@kubernetes/client-node";
+
+import { k8sListNamespacesOrError } from "@opstrace/kubernetes";
+import { getGKEKubeconfig } from "@opstrace/gcp";
+
+import { setAWSRegion, getEKSKubeconfig } from "@opstrace/aws";
+
+import { log, SECOND, retryUponAnyError } from "@opstrace/utils";
+
+import {
+  set as saveControllerConfig,
+  fetch as getControllerConfig
+} from "@opstrace/controller-config";
+
+import { rootReducer /*, State */ } from "./reducer";
+import { ClusterUpgradeTimeoutError } from "./errors";
+import { runInformers, blockUntilCacheHydrated } from "./informers";
+import { upgradeProgressReporter, waitForControllerDeployment } from "./readiness";
+
+// Note: a largish number of attempts as long as micro retries are not yet
+// implemented carefully and thoughtfully.
+const UPGRADE_ATTEMPTS = 5;
+
+// timeout per cluster upgrade attempt
+const UPGRADE_ATTEMPT_TIMEOUT_SECONDS = 60 * 20;
+
+interface UpgradeConfigInterface {
+  cloudProvider: "gcp" | "aws";
+  clusterName: string;
+  gcpProjectID: string | undefined;
+  gcpRegion: string | undefined;
+  awsRegion: string | undefined;
+}
+
+// think of this as singleton (set once, immutable, read from everywhere)
+let upgradeConfig: UpgradeConfigInterface;
+
+export function setUpgradeConfig(c: UpgradeConfigInterface): void {
+  upgradeConfig = c;
+
+  // Configure AWS client lib state with region information. Needs to be done
+  // before using any AWS client lib (S3, EKS, ...). Could be done by the code
+  // that also calls `setUpgradeConfig()` but on the other hand, this can
+  // also be done in here, which is a little cleaner.
+  if (c.awsRegion !== undefined) {
+    log.debug(
+      "setUpgradeConfig(): set AWS region for client libs: %s",
+      c.awsRegion
+    );
+    setAWSRegion(c.awsRegion);
+  }
+}
+
+async function getKubecfgIfk8sClusterExists(
+  upgradeConfig: UpgradeConfigInterface
+): Promise<KubeConfig | undefined> {
+  switch (upgradeConfig.cloudProvider) {
+    case "gcp":
+      return getGKEKubeconfig(upgradeConfig.clusterName);
+    case "aws":
+      return getEKSKubeconfig(
+        upgradeConfig.awsRegion!,
+        upgradeConfig.clusterName
+      );
+    default:
+      throw Error("must never be here");
+  }
+}
+
+function* upgradeClusterCore() {
+  if (upgradeConfig === undefined) {
+    throw new Error("call setUpgradeConfig() first");
+  }
+
+  const kubeconfig: KubeConfig | undefined = yield call(
+    getKubecfgIfk8sClusterExists,
+    upgradeConfig
+  );
+
+  if (kubeconfig === undefined) {
+    throw new Error("could not fetch cluster kubeconfig");
+  }
+
+  yield call(triggerUpgrade, kubeconfig);
+
+  log.info(
+    "Opstrace cluster upgrade done for %s (%s)",
+    upgradeConfig.clusterName,
+    upgradeConfig.cloudProvider
+  );
+}
+
+/**
+ * Trigger soft-upgrade by bumping the controller deployment version.
+ */
+function* triggerUpgrade(kubeConfig: KubeConfig) {
+  // Explicitly test the availability of the k8s api and exit if interaction
+  // fails.
+  yield call(k8sListNamespacesOrError, kubeConfig);
+
+  log.info("k8s cluster seems to exist, trigger upgrade");
+
+  log.info("Get current controller config map");
+  const ccfg = yield call(getControllerConfig, kubeConfig);
+  if (ccfg === undefined) {
+    throw new Error("could not get controller config map");
+  }
+
+  // TODO
+  if (ccfg.terminate) {
+    yield call(saveControllerConfig, ccfg, kubeConfig);
+  }
+
+  log.info("starting kubernetes informers");
+  const informers = yield fork(runInformers, kubeConfig);
+
+  yield call(blockUntilCacheHydrated);
+
+  yield call(waitForControllerDeployment);
+
+  log.info('wait for upgrade to complete ("wait for deployments/..." phase)');
+  yield call(upgradeProgressReporter);
+
+  // Cancel the forked informers so we can exit
+  yield cancel(informers);
+}
+
+/**
+ * Timeout control around a single cluster upgrade attempt.
+ */
+function* upgradeClusterAttemptWithTimeout() {
+  log.debug("upgradeClusterAttemptWithTimeout");
+  const { timeout } = yield race({
+    upgrade: call(upgradeClusterCore),
+    timeout: delay(UPGRADE_ATTEMPT_TIMEOUT_SECONDS * SECOND)
+  });
+
+  if (timeout) {
+    // Note that in this case redux-saga guarantees to have cancelled the
+    // task(s) that lost the race, i.e. the `upgrade` task above.
+    // see https://redux-saga.js.org/docs/advanced/TaskCancellation.html
+    // however, this does not seem to reliable cancel all tasks spawned along
+    // the hierarchy, maybe as of usage of promises as part of the stack?
+    // also see opstrace-prelaunch/issues/1457
+    log.warning(
+      "cluster upgrade attempt timed out after %s seconds",
+      UPGRADE_ATTEMPT_TIMEOUT_SECONDS
+    );
+    throw new ClusterUpgradeTimeoutError();
+  }
+}
+
+function* rootTaskUpgrade() {
+  // Note: a longish delay between attempts with the intention to give upgrade
+  // attempts some time to take effect holistically (sometimes, that is our
+  // impression, deletion of an individual resource might be confirmed
+  // synchronously, but after all still takes a while to be properly reflected
+  // across all views).
+  yield call(retryUponAnyError, {
+    task: upgradeClusterAttemptWithTimeout,
+    maxAttempts: UPGRADE_ATTEMPTS,
+    doNotLogDetailForTheseErrors: [ClusterUpgradeTimeoutError],
+    actionName: "cluster upgrade",
+    delaySeconds: 30
+  });
+}
+
+/**
+ * Entry point for cluster upgrade to be called by CLI.
+ */
+export async function upgradeCluster(
+  smOnError: (e: Error, detail: unknown) => void
+): Promise<void> {
+  const sm = createSagaMiddleware({ onError: smOnError });
+  createStore(rootReducer, applyMiddleware(sm));
+
+  await sm.run(rootTaskUpgrade).toPromise();
+
+  // this is helpful when the runtime is supposed to crash but doesn't
+  log.debug("end of upgradeCluster()");
+}
---
+++ packages/upgrader/src/readiness.ts
@@ -0,0 +1,213 @@
+/**
+ * Copyright 2020 Opstrace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { select, delay } from "redux-saga/effects";
+
+import { log, SECOND } from "@opstrace/utils";
+import {
+  activeDaemonsets,
+  activeDeployments,
+  activeStatefulsets,
+  activeCertificates
+} from "@opstrace/kubernetes";
+import { CONTROLLER_NAME } from "@opstrace/controller";
+
+import { State } from "./reducer";
+
+//eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function* waitForControllerDeployment() {
+
+  // Exit if controller deployment does not exist.
+  const state: State = yield select();
+  const { Deployments } = state.kubernetes.cluster;
+  const cd = Deployments.resources.find(d => d.name === CONTROLLER_NAME);
+  if (cd === undefined) {
+    throw new Error("controller deployment not found");
+  }
+
+  log.info("controller deployment found in informer-based state");
+
+  // follow controller deployment. .spec / .resources has the `status` property
+  // which may look like this:
+
+  //    "status": {
+  //      "observedGeneration": 1,
+  //      "replicas": 1,
+  //      "updatedReplicas": 1,
+  //      "readyReplicas": 1,
+  //      "availableReplicas": 1,
+  //      "conditions": [
+  //        {
+  //          "type": "Available",
+  //          "status": "True",
+  //          "lastUpdateTime": "2020-11-20T00:22:04Z",
+  //          "lastTransitionTime": "2020-11-20T00:22:04Z",
+  //          "reason": "MinimumReplicasAvailable",
+  //          "message": "Deployment has minimum availability."
+  //        },
+  //        {
+  //          "type": "Progressing",
+  //          "status": "True",
+  //          "lastUpdateTime": "2020-11-20T00:22:04Z",
+  //          "lastTransitionTime": "2020-11-20T00:21:37Z",
+  //          "reason": "NewReplicaSetAvailable",
+  //          "message": "ReplicaSet \"opstrace-controller-6775677449\" has successfully progressed."
+  //        }
+  //      ]
+  //    }
+  //  },
+
+  while (true) {
+    const state: State = yield select();
+    const { Deployments } = state.kubernetes.cluster;
+    const cd = Deployments.resources.find(d => d.name === CONTROLLER_NAME);
+
+    if (cd === undefined) {
+      log.debug("`cd` is undefined: should not happen in this loop");
+      yield delay(5000);
+      continue;
+    }
+
+    if (cd.spec.status === undefined) {
+      log.info("controller deployment: no status yet, wait");
+      yield delay(3000);
+      continue;
+    }
+
+    // debug-log the entire structure
+    log.debug(
+      "controller deployment status: %s",
+      JSON.stringify(cd.spec.status, null, 2)
+    );
+
+    if (cd.spec.status.conditions === undefined) {
+      log.info("controller deployment: no condition updates yet, wait");
+      yield delay(3000);
+      continue;
+    }
+
+    // example:
+    // [
+    //   {
+    //     "type": "Available",
+    //     "status": "False",
+    //     "lastUpdateTime": "2020-11-20T17:18:08Z",
+    //     "lastTransitionTime": "2020-11-20T17:18:08Z",
+    //     "reason": "MinimumReplicasUnavailable",
+    //     "message": "Deployment does not have minimum availability."
+    //   },
+    //   {
+    //     "type": "Progressing",
+    //     "status": "True",
+    //     "lastUpdateTime": "2020-11-20T17:18:09Z",
+    //     "lastTransitionTime": "2020-11-20T17:18:08Z",
+    //     "reason": "ReplicaSetUpdated",
+    //     "message": "ReplicaSet \"opstrace-controller-5584b7996f\" is progressing."
+    //   }
+    // ]
+
+    for (const cond of cd.spec.status.conditions) {
+      log.info(
+        `controller deployment: ${cond.type}: ${cond.status} (${cond.message})`
+      );
+    }
+
+    log.info(
+      "controller deployment: replicas: %s, of which are READY: %s",
+      cd.spec.status?.replicas,
+      cd.spec.status?.readyReplicas || 0
+    );
+
+    const rr = cd.spec.status?.readyReplicas;
+
+    if (rr !== undefined && rr >= 1) {
+      log.info(
+        "at least one replica is READY: desired state reached, continue"
+      );
+      break;
+    }
+    yield delay(15000);
+  }
+}
+
+//eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function* upgradeProgressReporter() {
+  while (true) {
+    const state: State = yield select();
+
+    const {
+      DaemonSets,
+      Deployments,
+      StatefulSets,
+      Certificates
+    } = state.kubernetes.cluster;
+
+    // Check DaemonSets
+    const activeDaemonSets = activeDaemonsets(DaemonSets.resources);
+
+    // Check Deployments
+    const activeDeploys = activeDeployments(Deployments.resources);
+
+    // Check StatefulSets
+    const activeStatefulSets = activeStatefulsets(StatefulSets.resources);
+
+    const activeCerts = activeCertificates(Certificates.resources);
+
+    log.info(`waiting for ${activeDeploys.length} Deployments`);
+    if (activeDeploys.length < 3) {
+      // for the last few: show the names.
+      for (const d of activeDeploys) {
+        log.debug("    %s", d);
+      }
+    }
+
+    log.info(`waiting for ${activeDaemonSets.length} DaemonSets`);
+    if (activeDaemonSets.length < 3) {
+      // for the last few: show the names.
+      for (const d of activeDaemonSets) {
+        log.debug("    %s", d);
+      }
+    }
+
+    log.info(`waiting for ${activeStatefulSets.length} StatefulSets`);
+    if (activeStatefulSets.length < 3) {
+      // for the last few: show the names.
+      for (const s of activeStatefulSets) {
+        log.debug("    %s", s);
+      }
+    }
+
+    log.info(`waiting for ${activeCerts.length} Certificates`);
+    if (activeCerts.length < 3) {
+      // for the last few: show the names.
+      for (const c of activeCerts) {
+        log.debug("    %s", c);
+      }
+    }
+
+    if (
+      activeDeploys.length +
+        activeDaemonSets.length +
+        activeStatefulSets.length +
+        activeCerts.length ===
+      0
+    ) {
+      break;
+    }
+
+    yield delay(5 * SECOND);
+  }
+}
