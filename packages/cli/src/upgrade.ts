e857f83d1f645965fc42fd0175bb2e5721dfd170
Simão Reis <sreis@opstrace.com>
2021-02-09 10:56:39 -0100

    cli: introduce upgrade command
    Signed-off-by: Simão Reis <sreis@opstrace.com>

---
+++ packages/cli/src/upgrade.ts
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2020 Opstrace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { log } from "@opstrace/utils";
+
+import * as cli from "./index";
+import * as util from "./util";
+
+export async function upgrade(): Promise<void> {
+  log.info(
+    `About to upgrade cluster ${cli.CLIARGS.clusterName} (${cli.CLIARGS.cloudProvider}).`
+  );
+
+  await util.promptForProceed();
+}
