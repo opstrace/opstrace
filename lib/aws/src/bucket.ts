3b1a54d966c5f2b8c4f438d90a2df4bd28338f99
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-02-10 14:19:33 +0100

    chore: copyright notice: 2020 -> 2019-2021
    find . \( ! -regex '.*/\..*' \) -type f -print0 | xargs -0 sed -i \
      's/Copyright 2020 Opstrace/Copyright 2019-2021 Opstrace/g'
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- lib/aws/src/bucket.ts
+++ lib/aws/src/bucket.ts
@@ -1,4 +1,4 @@
-// Copyright 2020 Opstrace, Inc.
+// Copyright 2019-2021 Opstrace, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /**
- * Copyright 2020 Opstrace, Inc.
+ * Copyright 2019-2021 Opstrace, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
