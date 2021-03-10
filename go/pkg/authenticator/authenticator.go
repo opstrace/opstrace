f9ad0f8fccee09238814482bc4d3f32fa43a6bd7
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-03-10 16:51:09 +0100

    go: authenticator: try to make code paths more tidy
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- go/pkg/authenticator/authenticator.go
+++ go/pkg/authenticator/authenticator.go
@@ -17,7 +17,6 @@ package authenticator
 import (
 	"fmt"
 	"net/http"
-	"strings"
 )
 
 // HTTP Request header used by GetTenant when disableAPIAuthentication is true
@@ -62,73 +61,43 @@ func GetTenant(
 	expectedTenantName *string,
 	disableAPIAuthentication bool,
 ) (string, bool) {
-	if expectedTenantName == nil {
-		// Tenant may vary on a per-request basis to this endpoint
-		if disableAPIAuthentication {
-			// TESTING: No single expected tenant, so check for tenant in the X-Scope-OrgID header
-			tenantName := r.Header.Get(TestTenantHeader)
-			if tenantName == "" {
-				exit401(w, fmt.Sprintf("missing test %s header specifying tenant", TestTenantHeader))
+
+	if expectedTenantName != nil {
+		if !disableAPIAuthentication {
+			// Authenticate and expect specific tenant. Otherwise send 401 response.
+			if !AuthenticateSpecificTenantByHeaderOr401(w, r, *expectedTenantName) {
 				return "", false
 			}
-			return tenantName, true
-		} else {
-			// Read/validate tenant from signed bearer token
-			authTokenUnverified, ok := getAPIAuthTokenUnverified(w, r)
-			if !ok {
-				return "", false
-			}
-			return getRequestAuthenticator(w, authTokenUnverified)
-		}
-	} else {
-		// Tenant must match configured value across all requests to this endpoint
-		if disableAPIAuthentication {
-			// TESTING: Just assume the expected tenant
-			return *expectedTenantName, true
-		} else {
-			// Validate the Authentication/Bearer token in the request and check that it has the expected tenant.
-			if !DataAPIRequestAuthenticator(w, r, *expectedTenantName) {
-				return "", false
-			}
+
+			// Request is authenticated as the expected tenant.
 			return *expectedTenantName, true
 		}
-	}
-}
 
-// Verifies that the tenant name embedded in the request's Authorization header is valid and matches the
-// expectedTenantName and returns true, or writes a 401 error to the response and returns false if the
-// tenant was invalid, not found, or didn't match expectedTenantName.
-func DataAPIRequestAuthenticator(w http.ResponseWriter, r *http.Request, expectedTenantName string) bool {
-	authTokenUnverified, ok := getAPIAuthTokenUnverified(w, r)
-	if !ok {
-		return false
-	}
-	return compareRequestAuthenticator(w, authTokenUnverified, expectedTenantName)
-}
-
-// Expect HTTP request to have a header of the shape
-//
-//      `Authorization: Bearer <AUTHTOKEN>` set.
-//
-// Extract and cryptographically verify authentication proof in HTTP request.
-// Emit error HTTP responses and return `false` upon any failure. Return `true`
-// only when the authentication proof is valid and matches the expected
-// Opstrace tenant name.
-func getAPIAuthTokenUnverified(w http.ResponseWriter, r *http.Request) (string, bool) {
-	// Read first value set for Authorization header. (no support for multiple
-	// of these headers yet, maybe never.)
-	av := r.Header.Get("Authorization")
-	if av == "" {
-		return "", exit401(w, "Authorization header missing")
-	}
-	asplits := strings.Split(av, "Bearer ")
-
-	if len(asplits) != 2 {
-		return "", exit401(w, "Authorization header format invalid. Expecting `Authorization: Bearer <AUTHTOKEN>`")
+		// ONLY FOR TESTING: do not inspect request, assume the expected tenant
+		return *expectedTenantName, true
 	}
 
-	authTokenUnverified := asplits[1]
-	return authTokenUnverified, true
+	// Do not expect specific tenant: allow for incoming requests to be
+	// authenticated with a variety of tenants.
+
+	if !disableAPIAuthentication {
+		// Authenticate (accept any tenant name). Otherwise send 401 response.
+		tnFromReq, ok := AuthenticateAnyTenantByHeaderOr401(w, r)
+		if !ok {
+			return "", false
+		}
+
+		return tnFromReq, true
+	}
+
+	// ONLY FOR TESTING: no single expected tenant, and authenticator
+	// is disabled: check for tenant in the X-Scope-OrgID header
+	tenantName := r.Header.Get(TestTenantHeader)
+	if tenantName == "" {
+		exit401(w, fmt.Sprintf("missing test %s header specifying tenant", TestTenantHeader))
+		return "", false
+	}
+	return tenantName, true
 }
 
 // Expect HTTP request to have URL containing a query parameter
@@ -151,25 +120,10 @@ func DDAPIRequestAuthenticator(w http.ResponseWriter, r *http.Request, expectedT
 	}
 
 	authTokenUnverified := apikey
-	return compareRequestAuthenticator(w, authTokenUnverified, expectedTenantName)
-}
 
-// Maybe call this authenticateOrWriteErrorResponse().
-func getRequestAuthenticator(w http.ResponseWriter, authTokenUnverified string) (string, bool) {
-	tenantNameFromToken, verr := validateAuthTokenGetTenantName(authTokenUnverified)
-
-	if verr != nil {
-		return "", exit401(w, verr.Error())
-	}
-
-	// Maybe change signature to return an error as second param, and nil here.
-	return tenantNameFromToken, true
-}
-
-func compareRequestAuthenticator(w http.ResponseWriter, authTokenUnverified string, expectedTenantName string) bool {
-	tenantNameFromToken, ok := getRequestAuthenticator(w, authTokenUnverified)
-	if !ok {
-		return false
+	tenantNameFromToken, veriferr := validateAuthTokenGetTenantName(authTokenUnverified)
+	if veriferr != nil {
+		return exit401(w, veriferr.Error())
 	}
 
 	if expectedTenantName != tenantNameFromToken {
