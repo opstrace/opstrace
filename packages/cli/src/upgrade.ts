ae1267698c3c84a5bf8218e42137a6361157079f
Simão Reis <sreis@opstrace.com>
2021-03-30 11:50:12 +0000

    wip: add flag for cluster config file to upgrade
    Signed-off-by: Simão Reis <sreis@opstrace.com>

--- packages/cli/src/upgrade.ts
+++ packages/cli/src/upgrade.ts
@@ -16,14 +16,50 @@
 
 import { log } from "@opstrace/utils";
 import { upgradeCluster, setUpgradeConfig } from "@opstrace/upgrader";
+import {
+  LatestAWSInfraConfigType,
+  LatestClusterConfigType,
+  LatestGCPInfraConfigType,
+  setClusterConfig
+} from "@opstrace/config";
 
 import * as cli from "./index";
 import * as util from "./util";
+import * as schemas from "./schemas";
+import * as ucc from "./ucc";
 
 export async function upgrade(): Promise<void> {
   log.info(
     `About to upgrade cluster ${cli.CLIARGS.clusterName} (${cli.CLIARGS.cloudProvider}).`
   );
+
+  const [userClusterConfig, infraConfigAWS, infraConfigGCP]: [
+    schemas.LatestClusterConfigFileSchemaType,
+    LatestAWSInfraConfigType | undefined,
+    LatestGCPInfraConfigType | undefined
+  ] = await ucc.uccGetAndValidate(
+    cli.CLIARGS.clusterConfigFilePath,
+    cli.CLIARGS.cloudProvider
+  );
+
+  // renderedClusterConfig: internal, complete
+  const renderedClusterConfig: LatestClusterConfigType = {
+    ...userClusterConfig,
+    ...{
+      aws: infraConfigAWS,
+      gcp: infraConfigGCP,
+      cloud_provider: cli.CLIARGS.cloudProvider,
+      cluster_name: cli.CLIARGS.clusterName,
+      // Important note. This field is intentionally set to the empty string
+      // during the upgrade since we do not want to override it. The value is
+      // instead read from the controller configuration stored in the config map.
+      tenant_api_authenticator_pubkey_set_json: ""
+    }
+  };
+
+  // make config for current context globally known in upgrader dirty
+  // cfg-as-singleton-immutable approach
+  setClusterConfig(renderedClusterConfig);
 
   let gcpProjectID: string | undefined;
   let gcpRegion: string | undefined;
