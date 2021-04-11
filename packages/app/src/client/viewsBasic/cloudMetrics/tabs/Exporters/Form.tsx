1f6a42fab52c7a2582fa8ba9c41819c1461bfde5
Nahum Wild <nahum@opstrace.com>
2021-04-12 09:52:47 +1200

    ui: basic exporters tab structure, copied from credentials
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

---
+++ packages/app/src/client/viewsBasic/cloudMetrics/tabs/Exporters/Form.tsx
@@ -0,0 +1,232 @@
+/**
+ * Copyright 2021 Opstrace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+
+import graphqlClient from "state/clients/graphqlClient";
+import { CondRender } from "client/utils/rendering";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import {
+  Input,
+  Select,
+  MenuItem,
+  FormControl,
+  FormLabel,
+  FormHelperText
+} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch"
+    }
+  }
+}));
+
+export function ExporterForm(props: { tenantId: string; onCreate: Function }) {
+  const { tenantId, onCreate } = props;
+  const { control, watch } = useForm({ defaultValues: { cloudProvider: "" } });
+  const cloudProvider = watch("cloudProvider");
+
+  console.log(tenantId);
+
+  return (
+    <Grid
+      container
+      alignItems="flex-start"
+      justify="flex-start"
+      direction="column"
+    >
+      <Grid item>
+        <FormControl>
+          <FormLabel>Add Exporter</FormLabel>
+          <Controller
+            render={({ field }) => (
+              <Select {...field}>
+                <MenuItem value={"aws"}>Amazon Web Services</MenuItem>
+                <MenuItem value={"gcp"}>Google Cloud Platform</MenuItem>
+              </Select>
+            )}
+            control={control}
+            name="cloudProvider"
+            defaultValue={10}
+          />
+        </FormControl>
+      </Grid>
+
+      <CondRender when={cloudProvider === "aws"}>
+        <AwsForm tenantId={tenantId} onCreate={onCreate} />
+      </CondRender>
+      <CondRender when={cloudProvider === "gcp"}>
+        <GcpForm tenantId={tenantId} onCreate={onCreate} />
+      </CondRender>
+    </Grid>
+  );
+}
+
+type ControlledInputProps = {
+  name: `${string}` | `${string}.${string}` | `${string}.${number}`;
+  label: string;
+  helperText?: string;
+  inputProps?: {};
+  control: any;
+};
+
+const ControlledInput = ({
+  name,
+  label,
+  inputProps = {},
+  helperText,
+  control
+}: ControlledInputProps) => (
+  <Grid item>
+    <Controller
+      render={({ field }) => (
+        <FormControl>
+          <FormLabel>{label}</FormLabel>
+          <Input {...field} {...inputProps} />
+          <CondRender when={helperText !== undefined}>
+            <FormHelperText>{helperText}</FormHelperText>
+          </CondRender>
+        </FormControl>
+      )}
+      control={control}
+      name={name}
+    />
+  </Grid>
+);
+
+const awsDefaultValues = {
+  name: "",
+  accessKeyId: "",
+  secretAccessKey: ""
+};
+
+function AwsForm(props: { tenantId: string; onCreate: Function }) {
+  const { tenantId, onCreate } = props;
+  const classes = useStyles();
+  const { handleSubmit, reset, control } = useForm({
+    defaultValues: awsDefaultValues
+  });
+  const onSubmit = (data: { name: string }) => {
+    graphqlClient
+      .CreateExporters({
+        exporters: {
+          tenant: tenantId,
+          name: data.name,
+          type: "aws"
+        }
+      })
+      .then(response => {
+        onCreate();
+        reset(awsDefaultValues);
+      });
+  };
+
+  return (
+    <form className={classes.root} onSubmit={handleSubmit(onSubmit)}>
+      <ControlledInput name="name" label="Name" control={control} />
+      <ControlledInput
+        name="accessKeyId"
+        label="Access Key ID"
+        control={control}
+      />
+      <ControlledInput
+        name="secretAccessKey"
+        label="Secret Access Key"
+        helperText="Important: this is stored as plain text."
+        control={control}
+      />
+
+      <Grid
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="flex-start"
+      >
+        <Grid item>
+          <button type="button" onClick={() => reset(awsDefaultValues)}>
+            Reset
+          </button>
+        </Grid>
+        <Grid item>
+          <input type="submit" />
+        </Grid>
+      </Grid>
+    </form>
+  );
+}
+
+const gcpDefaultValues = {
+  name: "",
+  accessDoc: ""
+};
+
+function GcpForm(props: { tenantId: string; onCreate: Function }) {
+  const { tenantId, onCreate } = props;
+  const classes = useStyles();
+  const { handleSubmit, reset, control } = useForm({
+    defaultValues: gcpDefaultValues
+  });
+
+  const onSubmit = (data: { name: string }) => {
+    graphqlClient
+      .CreateExporters({
+        exporters: {
+          tenant: tenantId,
+          name: data.name,
+          type: "gcp"
+        }
+      })
+      .then(response => {
+        onCreate();
+        reset(awsDefaultValues);
+      });
+  };
+
+  return (
+    <form className={classes.root} onSubmit={handleSubmit(onSubmit)}>
+      <ControlledInput name="name" label="Name" control={control} />
+      <ControlledInput
+        name="accessDoc"
+        label="Access Doc"
+        inputProps={{ multiline: true }}
+        helperText="Important: this is stored as plain text."
+        control={control}
+      />
+
+      <Grid
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="flex-start"
+      >
+        <Grid item>
+          <button type="button" onClick={() => reset(gcpDefaultValues)}>
+            Reset
+          </button>
+        </Grid>
+        <Grid item>
+          <input type="submit" />
+        </Grid>
+      </Grid>
+    </form>
+  );
+}
---
+++ packages/app/src/client/viewsBasic/cloudMetrics/tabs/Exporters/index.tsx
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2021 Opstrace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import useFetcher from "client/hooks/useFetcher";
+
+import { ExportersTable } from "./Table";
+import { ExporterForm } from "./Form";
+
+import { Box } from "client/components/Box";
+import Grid from "@material-ui/core/Grid";
+
+const Exporters = () => {
+  const { tenantId } = useParams<{ tenantId: string }>();
+
+  const { data, mutate: changeCallback } = useFetcher(
+    `query exporters($tenant_id: String!) {
+       exporter(where: {tenant: {_eq: $tenant_id}}) {
+         tenant
+         name
+         type
+         config
+         credentialByCredentialTenant {
+           name
+         }
+         created_at
+       }
+     }`,
+    { tenant_id: tenantId }
+  );
+
+  return (
+    <Box display="flex" height="500px" width="700px">
+      <Grid
+        container
+        alignItems="flex-start"
+        justify="flex-start"
+        direction="column"
+      >
+        <Grid item>
+          <ExportersTable
+            tenantId={tenantId}
+            onChange={changeCallback}
+            rows={data?.exporter}
+          />
+        </Grid>
+        <Grid item>
+          <ExporterForm tenantId={tenantId} onCreate={changeCallback} />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+const ExportersTab = {
+  key: "exporters",
+  label: "Exporters",
+  content: Exporters
+};
+
+export { Exporters, ExportersTab };
