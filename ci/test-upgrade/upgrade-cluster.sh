6e8bd7d82e66820544728a6f7477e53a115009cc
Simão Reis <sreis@opstrace.com>
2021-04-12 09:02:16 +0000

    ci: upgrades - add path to cluster config
    Signed-off-by: Simão Reis <sreis@opstrace.com>

--- ci/test-upgrade/upgrade-cluster.sh
+++ ci/test-upgrade/upgrade-cluster.sh
@@ -9,6 +9,7 @@ source ci/utils.sh
 # into the cluster and wait until deployments are 'ready'.
 echo "--- upgrading cluster"
 ./to/opstrace upgrade ${OPSTRACE_CLOUD_PROVIDER} ${OPSTRACE_CLUSTER_NAME} \
+    --cluster-config ci/cluster-config.yaml \
     --log-level=debug \
     --yes
 
