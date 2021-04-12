b5b7e54fcfa66cc78f038cd01a727ea6317b29d2
Nahum Wild <nahum@opstrace.com>
2021-04-12 13:28:23 +1200

    ui: exporter row expansion to show config
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- packages/app/src/client/viewsBasic/cloudMetrics/tabs/Exporters/Table.tsx
+++ packages/app/src/client/viewsBasic/cloudMetrics/tabs/Exporters/Table.tsx
@@ -29,6 +29,13 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import Box from "@material-ui/core/Box";
+
 const useStyles = makeStyles({
   table: {
     minWidth: 650
@@ -40,6 +47,7 @@ type Row = {
   type: string;
   credential: string;
   created_at: string;
+  config: string;
 };
 
 type ExportersTableProps = {
@@ -82,22 +90,57 @@ export const ExportersTable = (props: ExportersTableProps) => {
         </TableHead>
         <TableBody>
           {rows.map(row => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell>{row.type}</TableCell>
-              <TableCell>{row.credential}</TableCell>
-              <TableCell>{format(parseISO(row.created_at), "Pppp")}</TableCell>
-              <TableCell>
-                <button type="button" onClick={() => deleteExporter(row.name)}>
-                  Delete
-                </button>
-              </TableCell>
-            </TableRow>
+            <ExportersRow key={row.name} row={row} onDelete={deleteExporter} />
           ))}
         </TableBody>
       </Table>
     </TableContainer>
+  );
+};
+
+const ExportersRow = (props: {
+  row: Row;
+  onDelete: (name: string) => void;
+}) => {
+  const { row, onDelete } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell>{row.type}</TableCell>
+        <TableCell>{row.credential}</TableCell>
+        <TableCell>{format(parseISO(row.created_at), "Pppp")}</TableCell>
+        <TableCell>
+          <button type="button" onClick={() => onDelete(row.name)}>
+            Delete
+          </button>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="subtitle1" gutterBottom component="div">
+                {row.type === "aws" ? "CloudWatch" : "Stackdriver"} Config
+              </Typography>
+              <pre>{row.config}</pre>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
   );
 };
