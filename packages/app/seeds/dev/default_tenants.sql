100d876aea38c04380d3bc209b1788c8e83ea44a
Nahum Wild <nahum@opstrace.com>
2021-05-05 08:10:59 +1200

    ui: removed test tenant "not for dns" from dev seed
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- packages/app/seeds/dev/default_tenants.sql
+++ packages/app/seeds/dev/default_tenants.sql
@@ -1,3 +1,2 @@
 INSERT INTO public.tenant (name, type) VALUES ('system', 'USER');
 INSERT INTO public.tenant (name, type) VALUES ('default', 'USER');
-INSERT INTO public.tenant (name, type) VALUES ('not for dns', 'USER');
