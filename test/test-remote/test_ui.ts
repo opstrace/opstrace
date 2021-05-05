f60066f0ed50fd08495155027bf69ca5a8d5b5e5
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-05-05 15:11:03 +0200

    test-remote: login flow in suite setup
    Perform login flow in suite setup and
    make cookies (authentication state)
    available for reusage.
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- test/test-remote/test_ui.ts
+++ test/test-remote/test_ui.ts
@@ -14,11 +14,17 @@
  * limitations under the License.
  */
 
+import { strict as assert } from "assert";
+
 import path from "path";
+
+import got from "got";
 
 import {
   log,
   globalTestSuiteSetupOnce,
+  httpTimeoutSettings,
+  logHTTPResponse,
   CLUSTER_BASE_URL,
   TEST_REMOTE_ARTIFACT_DIRECTORY
 } from "./testutils";
@@ -27,14 +33,82 @@ import {
 // this does not work, too late probably. set via Makefile entrypoint
 // process.env.DEBUG = "pw:api";
 
-import type { ChromiumBrowser } from "playwright";
+import type { ChromiumBrowser, Browser, Cookie } from "playwright";
 import { chromium } from "playwright";
 
-let browser: ChromiumBrowser;
+let BROWSER: ChromiumBrowser;
 
 // Generate and return a path in the artifact directory
 function artipath(filename: string) {
   return path.join(TEST_REMOTE_ARTIFACT_DIRECTORY, filename);
+}
+
+// A data structure like this:
+// 2021-05-05T12:16:05.105Z info: cookies:
+// [
+//   {
+//     "sameSite": "None",
+//     "name": "_legacy_auth0.is.authenticated",
+//     "value": "true",
+//     "domain": "jpload-1620131593.opstrace.io",
+//     "path": "/",
+//     "expires": 1620303358,
+//     "httpOnly": false,
+//     "secure": true
+//   },
+//   {
+// ...
+// ]
+let COOKIES_AFTER_LOGIN: Cookie[] | undefined;
+
+async function performLoginFlow(br: Browser) {
+  log.info("browser.newContext()");
+  const context = await br.newContext({ ignoreHTTPSErrors: true });
+  log.info("context.newPage()");
+  const page = await context.newPage();
+
+  log.info("page.goto(%s)", CLUSTER_BASE_URL);
+  await page.goto(CLUSTER_BASE_URL);
+  log.info("`load` event");
+
+  // <button class="MuiButtonBase-root Mui... MuiButton-sizeLarge" tabindex="0" type="button">
+  // <span class="MuiButton-label">Log in</span>
+  log.info('page.waitForSelector("css=button")');
+  await page.waitForSelector("css=button");
+
+  log.info("page.screenshot()");
+  await page.screenshot({
+    path: artipath("uishot-rootpage.png")
+  });
+
+  log.info('page.click("text=Log in")');
+  await page.click("text=Log in");
+
+  // Wait for CI-specific username/pw login form to appear
+  await page.waitForSelector("text=Don't remember your password?");
+
+  await page.fill("css=input[type=email]", "ci-test@opstrace.com");
+  await page.fill("css=input[type=password]", "This-is-not-a-secret!");
+
+  await page.screenshot({
+    path: artipath("uishot-auth0-login-page.png")
+  });
+
+  await page.click("css=button[type=submit]");
+
+  // The first view after successful login is expected to be the details page
+  // for the `system` tenant, showing a link to Grafana.
+  await page.waitForSelector("text=Grafana:");
+
+  await page.screenshot({
+    path: artipath("uishot-after-auth0-login.png")
+  });
+
+  const cookies = await context.cookies(CLUSTER_BASE_URL);
+  log.info("cookies:\n%s", JSON.stringify(cookies, null, 2));
+
+  // expose globally
+  COOKIES_AFTER_LOGIN = cookies;
 }
 
 suite("test_ui_with_headless_browser", function () {
@@ -43,7 +117,8 @@ suite("test_ui_with_headless_browser", function () {
     globalTestSuiteSetupOnce();
 
     log.info("chromium.launch()");
-    browser = await chromium.launch({
+    BROWSER = await chromium.launch({
+      headless: false, // set to false to see browser on your desktop
       args: [
         // https://github.com/microsoft/playwright/blob/761bd78879c83ed810ae38ef39513b2d874badb1/docs/ci.md#docker
         "--disable-dev-shm-usage",
@@ -52,57 +127,20 @@ suite("test_ui_with_headless_browser", function () {
       ]
     });
 
+    await performLoginFlow(BROWSER);
+
+    // Perform login flow in setup, so that the authentications state (namely,
+    // cookies), can be re-used in individual tests.
+
     log.info("suite setup done");
   });
 
   suiteTeardown(async function () {
     log.info("suite teardown");
     log.info("browser.close()");
-    await browser.close();
+    await BROWSER.close();
     log.info("suite teardown done");
   });
 
-  test("playwright", async function () {
-    log.info("browser.newContext()");
-    const context = await browser.newContext({ ignoreHTTPSErrors: true });
-    log.info("context.newPage()");
-    const page = await context.newPage();
-
-    log.info("page.goto(%s)", CLUSTER_BASE_URL);
-    await page.goto(CLUSTER_BASE_URL);
-    log.info("`load` event");
-
-    // <button class="MuiButtonBase-root Mui... MuiButton-sizeLarge" tabindex="0" type="button">
-    // <span class="MuiButton-label">Log in</span>
-    log.info('page.waitForSelector("css=button")');
-    await page.waitForSelector("css=button");
-
-    log.info("page.screenshot()");
-    await page.screenshot({
-      path: artipath("uishot-rootpage.png")
-    });
-
-    log.info('page.click("text=Log in")');
-    await page.click("text=Log in");
-
-    // Wait for CI-specific username/pw login form to appear
-    await page.waitForSelector("text=Don't remember your password?");
-
-    await page.fill("css=input[type=email]", "ci-test@opstrace.com");
-    await page.fill("css=input[type=password]", "This-is-not-a-secret!");
-
-    await page.screenshot({
-      path: artipath("uishot-auth0-login-page.png")
-    });
-
-    await page.click("css=button[type=submit]");
-
-    // The first view after successful login is expected to be the details page
-    // for the `system` tenant, showing a link to Grafana.
-    await page.waitForSelector("text=Grafana:");
-
-    await page.screenshot({
-      path: artipath("uishot-after-auth0-login.png")
-    });
-  });
+  test("create_tenant_api", async function () {});
 });
