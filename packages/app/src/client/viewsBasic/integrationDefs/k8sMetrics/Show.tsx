de233979ee26bb3c007a7402dce984a10c9946bd
Nahum Wild <nahum@opstrace.com>
2021-05-10 15:35:30 +1200

    ui: nicer integration show page based on tenant detail. still sort of a place holder
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- packages/app/src/client/viewsBasic/integrationDefs/k8sMetrics/Show.tsx
+++ packages/app/src/client/viewsBasic/integrationDefs/k8sMetrics/Show.tsx
@@ -17,11 +17,50 @@
 import React from "react";
 
 import { IntegrationProps } from "client/viewsBasic/tenantIntegrations/utils";
+import { IntegrationDefProps } from "client/viewsBasic/integrationDefs/utils";
 
-export const K8sMetricsShow = ({ integration }: IntegrationProps) => {
+import { Box } from "client/components/Box";
+import Attribute from "client/components/Attribute";
+import { Card, CardContent, CardHeader } from "client/components/Card";
+
+export const K8sMetricsShow = ({
+  integration,
+  integrationDef
+}: IntegrationProps & IntegrationDefProps) => {
   return (
-    <div>
-      Show Integration - {integration.name} - {integration.kind}
-    </div>
+    <Box
+      width="100%"
+      height="100%"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexWrap="wrap"
+      p={1}
+    >
+      <Box maxWidth={700}>
+        <Card p={3}>
+          <CardHeader
+            titleTypographyProps={{ variant: "h5" }}
+            title={integration.name}
+          />
+          <CardContent>
+            <Box display="flex">
+              <Box display="flex" flexDirection="column">
+                <Attribute.Key>Kind:</Attribute.Key>
+                <Attribute.Key>Category:</Attribute.Key>
+                <Attribute.Key>Status:</Attribute.Key>
+                <Attribute.Key>Created:</Attribute.Key>
+              </Box>
+              <Box display="flex" flexDirection="column" flexGrow={1}>
+                <Attribute.Value>{integrationDef.kind}</Attribute.Value>
+                <Attribute.Value>{integrationDef.category}</Attribute.Value>
+                <Attribute.Value>{integration.status}</Attribute.Value>
+                <Attribute.Value>{integration.created_at}</Attribute.Value>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
+    </Box>
   );
 };
