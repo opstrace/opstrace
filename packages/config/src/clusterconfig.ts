3ad06aee7dc655bfb9fc633eddab716291c2d34c
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-05-11 15:51:08 +0200

    cli: improve tenant name regex
    It is important to match the regex
    beginning to end!
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- packages/config/src/clusterconfig.ts
+++ packages/config/src/clusterconfig.ts
@@ -30,7 +30,7 @@ export const CLUSTER_NAME_REGEX = /^[a-z0-9-_]+$/;
 
 // This is the same regex the k8s ecosystem uses for dns-related label values
 // see https://github.com/opstrace/opstrace/issues/710
-export const TENANT_NAME_REGEX = /[a-z0-9]([-a-z0-9]*[a-z0-9])?/;
+export const TENANT_NAME_REGEX = /^[a-z0-9]([-a-z0-9]*[a-z0-9])?$/;
 
 export function setClusterConfig(c: LatestClusterConfigType): void {
   if (clusterConfig !== undefined) {
