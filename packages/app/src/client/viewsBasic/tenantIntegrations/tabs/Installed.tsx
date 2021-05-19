f20f83779de6a7881d9aed296b9df303f905e549
Mat Appelman <mat@opstrace.com>
2021-05-19 10:43:01 -0700

    chore: rename integration status component
    Signed-off-by: Mat Appelman <mat@opstrace.com>

--- packages/app/src/client/viewsBasic/tenantIntegrations/tabs/Installed.tsx
+++ packages/app/src/client/viewsBasic/tenantIntegrations/tabs/Installed.tsx
@@ -37,6 +37,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+
 // Temporary direct import
 import IntegrationStatus from "client/viewsBasic/integrationDefs/k8sMetrics/Status";
 
