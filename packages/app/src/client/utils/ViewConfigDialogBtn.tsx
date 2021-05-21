8c453e57d94c3052bd36f4e2ec989e8c2e81bb67
Nahum Wild <nahum@opstrace.com>
2021-05-21 14:59:46 +1200

    ui: switch i9n "view config" modal to being a dialog that positions correctly on screen. not formatting yaml yet
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

---
+++ packages/app/src/client/utils/ViewConfigDialogBtn.tsx
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2021 Opstrace, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+
+import { Button } from "client/components/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+export const ViewConfigDialogBtn = ({
+  filename,
+  config
+}: {
+  filename: string;
+  config: string;
+}) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const descriptionElementRef = React.useRef<HTMLElement>(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <>
+      <Button variant="outlined" size="small" onClick={handleOpen}>
+        View YAML
+      </Button>
+      <Dialog open={open} onClose={handleClose} scroll="paper" maxWidth="lg">
+        <DialogTitle id="scroll-dialog-title">{filename}</DialogTitle>
+        <DialogContent dividers={true}>
+          <DialogContentText
+            id="i9n-config"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            <pre>{config}</pre>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
