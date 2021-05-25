6d8d04850780f2d7e9e45c7ed54602de4415d3f6
Nahum Wild <nahum@opstrace.com>
2021-05-25 16:52:58 +1200

    ui: removing accidentially commited migrations that were breaking upgrading a cluster with defined integrations
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- packages/app/migrations/1621731892198_alter_table_public_integration_add_column_tenant_name/up.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "public"."integration" ADD COLUMN "tenant_name" text NOT NULL;
--- packages/app/migrations/1621731969932_alter_table_public_integration_drop_column_tenant_name/down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "public"."integration" ADD COLUMN "tenant_name" text;
-ALTER TABLE "public"."integration" ALTER COLUMN "tenant_name" DROP NOT NULL;
