d41a4777ad4525707f1dfb1170b954c823caf4ac
Simão Reis <sreis@opstrace.com>
2021-05-25 17:34:19 +0000

    ci(cd): add missing tenant token files
    Signed-off-by: Simão Reis <sreis@opstrace.com>

--- ci/cd/fetch-secrets.sh
+++ ci/cd/fetch-secrets.sh
@@ -4,7 +4,9 @@ aws s3 cp "s3://buildkite-managedsecretsbucket-100ljuov8ugv2/" secrets/ --recurs
     --include "aws-dev-svc-acc-env.sh" \
     --include "aws-loadtest-acc-env.sh" \
     --include "dns-service-login-for-ci.json" \
-    --include "cdtest-tenant-api-token-default"
+    --include "cdtest-tenant-api-token-default" \
+    --include "cdtest-tenant-api-token-system"
 
 echo "-- renaming tenant api token filename required by test-remote"
 mv secrets/cdtest-tenant-api-token-default tenant-api-token-default
+mv secrets/cdtest-tenant-api-token-system tenant-api-token-system
