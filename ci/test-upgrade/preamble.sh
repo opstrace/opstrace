4882c980b9090c8c28a3478c33593363ff6363ff
Simão Reis <sreis@opstrace.com>
2021-05-26 15:45:08 +0000

    ci(upgrades): add preamble step
    Signed-off-by: Simão Reis <sreis@opstrace.com>

---
+++ ci/test-upgrade/preamble.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+make rebuild-ci-container-image
+# testrunner run tsc which requires buildinfo package to be set
+make set-build-info-constants
+make rebuild-testrunner-container-images
+make rebuild-looker-container-images
