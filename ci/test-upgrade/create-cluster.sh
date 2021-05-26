735d5d56106dcfdd6e7c28d0c0a9e4c324c00d85
Simão Reis <sreis@opstrace.com>
2021-05-26 15:37:30 +0000

    ci(upgrades): add gcp to pipeline
    Signed-off-by: Simão Reis <sreis@opstrace.com>

--- ci/test-upgrade/create-cluster.sh
+++ ci/test-upgrade/create-cluster.sh
@@ -5,16 +5,20 @@ set -eou pipefail
 # Import helper functions.
 source ci/utils.sh
 
-# Run opstrace installer locally. The installer will deploy the controller into
-# the cluster and wait until deployments are 'ready'.
+if [[ "${OPSTRACE_CLOUD_PROVIDER}" == "gcp" ]]; then
+    echo "--- cloud auth activate-service-account: ${GOOGLE_APPLICATION_CREDENTIALS}"
+    # Inside the CI container, log in to GCP with service account credentials.
+    gcloud auth activate-service-account \
+        --key-file=${GOOGLE_APPLICATION_CREDENTIALS} \
+        --project ${OPSTRACE_GCP_PROJECT_ID}
+fi
+
 echo "--- creating cluster"
 ./from/opstrace create ${OPSTRACE_CLOUD_PROVIDER} ${OPSTRACE_CLUSTER_NAME} \
     --cluster-config ci/cluster-config.yaml \
     --log-level=debug \
+    --write-kubeconfig-file=${OPSTRACE_KUBECONFIG} \
     --yes
-
-echo "--- configuring kubectl"
-configure_kubectl_aws_or_gcp
 
 echo "--- checking cluster is using certificate issued by LetsEncrypt"
 retry_check_certificate loki.system.${OPSTRACE_CLUSTER_NAME}.opstrace.io:443
--- ci/test-upgrade/teardown.sh
+++ ci/test-upgrade/teardown.sh
@@ -2,8 +2,17 @@
 
 set -eou pipefail
 
+case ${OPSTRACE_CLOUD_PROVIDER} in
+    aws)
+        REGION=${AWS_CLI_REGION}
+        ;;
+    gcp)
+        REGION=${GCLOUD_CLI_REGION}
+        ;;
+esac
+
 echo "--- tearing down cluster"
 ./to/opstrace destroy ${OPSTRACE_CLOUD_PROVIDER} ${OPSTRACE_CLUSTER_NAME} \
-    --region=${AWS_CLI_REGION} \
+    --region=${REGION} \
     --log-level=debug\
     --yes
