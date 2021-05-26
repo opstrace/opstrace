03283f52fdb9430cd8ef70ab0b2d41fef9a1708c
Simão Reis <sreis@opstrace.com>
2021-05-26 15:45:08 +0000

    ci(upgrades): add preamble step
    Signed-off-by: Simão Reis <sreis@opstrace.com>

--- ci/test-upgrade/run.sh
+++ ci/test-upgrade/run.sh
@@ -2,13 +2,24 @@
 
 set -eou pipefail
 
-make rebuild-ci-container-image
-# testrunner run tsc which requires buildinfo package to be set
-make set-build-info-constants
-make rebuild-testrunner-container-images
-make rebuild-looker-container-images
-
 make ci-fetch-secrets
+
+make ci-testupgrade-fetch-cli-artifacts
+make ci-testupgrade-dns-service-credentials
+
+case "${OPSTRACE_CLOUD_PROVIDER}" in
+    aws)
+        # Note: sourcing this file exports AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+        source secrets/aws-dev-svc-acc-env.sh
+        ;;
+    gcp)
+        # Shard across GCP CI projects.
+        #export OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-aaa ci-shard-bbb ci-shard-ccc)
+        export OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-ddd ci-shard-eee ci-shard-fff)
+        echo "--- random choice for GCP project ID: ${OPSTRACE_GCP_PROJECT_ID}"
+        export GOOGLE_APPLICATION_CREDENTIALS=./secrets/gcp-svc-acc-${OPSTRACE_GCP_PROJECT_ID}.json
+        ;;
+esac
 
 # Override the target kubeconfig directory to point to the checkout directory.
 # Account for cloud provider to allow multiple simultaneous runs of this build.
@@ -16,19 +27,6 @@ export OPSTRACE_KUBE_CONFIG_HOST=$(pwd)/${OPSTRACE_CLOUD_PROVIDER}/.kube
 export OPSTRACE_KUBECONFIG=$(pwd)/${OPSTRACE_CLOUD_PROVIDER}/.kube/config
 mkdir -p $(pwd)/${OPSTRACE_CLOUD_PROVIDER}/.kube/
 
-# Note: sourcing this file exports AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-source secrets/aws-dev-svc-acc-env.sh
-
-if [[ "${OPSTRACE_CLOUD_PROVIDER}" == "gcp" ]]; then
-    # Shard across GCP CI projects.
-    #export OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-aaa ci-shard-bbb ci-shard-ccc)
-    export OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-ddd ci-shard-eee ci-shard-fff)
-    echo "--- random choice for GCP project ID: ${OPSTRACE_GCP_PROJECT_ID}"
-    export GOOGLE_APPLICATION_CREDENTIALS=./secrets/gcp-svc-acc-${OPSTRACE_GCP_PROJECT_ID}.json
-fi
-
-make ci-testupgrade-fetch-cli-artifacts
-make ci-testupgrade-dns-service-credentials
 
 teardown() {
     LAST_EXIT_CODE=$?
