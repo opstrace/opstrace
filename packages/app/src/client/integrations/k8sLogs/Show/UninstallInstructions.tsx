d893f72969889414cd139f32a0fd3cf0e6a7f8eb
Nahum Wild <nahum@opstrace.com>
2021-05-28 09:25:01 +1200

    ui: for integrations renamed "delete" to "uninstall"
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- packages/app/src/client/integrations/k8sLogs/Show/UninstallInstructions.tsx
+++ packages/app/src/client/integrations/k8sLogs/Show/UninstallInstructions.tsx
@@ -24,6 +24,7 @@ import * as commands from "./templates/commands";
 import * as grafana from "client/utils/grafana";
 
 import { CopyToClipboardIcon } from "client/components/CopyToClipboard";
+
 import { ViewConfigDialogBtn } from "client/integrations/common/ViewConfigDialogBtn";
 import { UninstallBtn } from "client/integrations/common/UninstallIntegrationBtn";
 
