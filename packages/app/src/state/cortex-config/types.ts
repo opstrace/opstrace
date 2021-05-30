1539652894d2328a8a3ce55b6b4d6d78843447df
Mat Appelman <mat@opstrace.com>
2021-05-29 17:28:32 -0700

    chore: add more validation to runtime config validator
    Signed-off-by: Mat Appelman <mat@opstrace.com>

--- packages/app/src/state/cortex-config/types.ts
+++ packages/app/src/state/cortex-config/types.ts
@@ -18,24 +18,28 @@ import * as yup from "yup";
 
 const isDuration = {
   name: "is-duration",
-  message:
-    // eslint-disable-next-line no-template-curly-in-string
-    "${path} is not a valid duration. Must be [number][s|m|h|d|w], e.g. 2d for 2 days.",
-  test: (value: any) => /^\d+[s|m|h|d|w]{1}$/.test(value || "")
+  message: "Must be a valid duration: [number][s|m|h|d|w], e.g. 2d for 2 days.",
+  test: (value: any) =>
+    typeof value === "undefined" || /^\d+[s|m|h|d|w]{1}$/.test(value || "") // allow this field to be undefined or be a valid duration
 };
 
 export const cortexLimitsSchema = yup.object({
-  ingestion_rate: yup.number().integer().meta({
+  ingestion_rate: yup.number().integer().min(0, "Cannot be negative").meta({
     description: "Ingestion rate limit in samples per second for this tenant."
   }),
-  ingestion_burst_size: yup.number().integer().meta({
-    description:
-      "Allowed ingestion burst size (in number of samples) for this tenant."
-  }),
-  drop_labels: yup.array().of(yup.string()).meta({
-    description:
-      "Comma-separated list of labels to remove from all metrics in this tenant."
-  }),
+  ingestion_burst_size: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "Allowed ingestion burst size (in number of samples) for this tenant."
+    }),
+  // Not including in first version. We can map to a comma separated text box for the values in the next iteration.
+  // drop_labels: yup.array().of(yup.string()).meta({
+  //   description:
+  //     "Comma-separated list of labels to remove from all metrics in this tenant."
+  // }),
   accept_ha_samples: yup.boolean().meta({
     description:
       "Enable handling of samples with external labels identifying replicas in an HA Prometheus setup."
@@ -48,24 +52,32 @@ export const cortexLimitsSchema = yup.object({
     description:
       "Prometheus label to look for in samples to identify a Prometheus HA replica."
   }),
-  ha_max_clusters: yup.number().integer().meta({
+  ha_max_clusters: yup.number().integer().min(0, "Cannot be negative").meta({
     description:
       "Maximum number of clusters that HA tracker will keep track of for single user."
   }),
   max_label_name_length: yup
     .number()
     .meta({ description: "Maximum length accepted for label names." }),
-  max_label_value_length: yup.number().integer().meta({
-    description:
-      "Maximum length accepted for label value. This setting also applies to the metric name."
-  }),
+  max_label_value_length: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "Maximum length accepted for label value. This setting also applies to the metric name."
+    }),
   max_label_names_per_series: yup
     .number()
     .meta({ description: "Maximum number of label names per series." }),
-  max_metadata_length: yup.number().integer().meta({
-    description:
-      "Maximum length accepted for metric metadata. Metadata refers to Metric Name, HELP and UNIT."
-  }),
+  max_metadata_length: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "Maximum length accepted for metric metadata. Metadata refers to Metric Name, HELP and UNIT."
+    }),
   reject_old_samples: yup
     .boolean()
     .meta({ description: "Reject old samples." }),
@@ -89,48 +101,88 @@ export const cortexLimitsSchema = yup.object({
   enforce_metric_name: yup
     .boolean()
     .meta({ description: "Enforce every sample to have a metric name." }),
-  ingestion_tenant_shard_size: yup.number().integer().meta({
-    description:
-      "The Tenant's shard size when the shuffle-sharding strategy is used. Must be set both on ingesters and distributors. A value of 0 disables shuffle sharding for the tenant."
-  }),
-  max_series_per_user: yup.number().integer().meta({
-    description:
-      "The maximum number of active series for this tenant, per ingester. 0 to disable."
-  }),
-  max_series_per_metric: yup.number().integer().meta({
-    description:
-      "The maximum number of active series per metric name, per ingester. 0 to disable."
-  }),
-  max_global_series_per_user: yup.number().integer().meta({
-    description:
-      "The maximum number of active series for this tenant, across the cluster. 0 to disable."
-  }),
-  max_global_series_per_metric: yup.number().integer().meta({
-    description:
-      "The maximum number of active series per metric name, across the cluster. 0 to disable."
-  }),
+  ingestion_tenant_shard_size: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "The Tenant's shard size when the shuffle-sharding strategy is used. Must be set both on ingesters and distributors. A value of 0 disables shuffle sharding for the tenant."
+    }),
+  max_series_per_user: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "The maximum number of active series for this tenant, per ingester. 0 to disable."
+    }),
+  max_series_per_metric: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "The maximum number of active series per metric name, per ingester. 0 to disable."
+    }),
+  max_global_series_per_user: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "The maximum number of active series for this tenant, across the cluster. 0 to disable."
+    }),
+  max_global_series_per_metric: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "The maximum number of active series per metric name, across the cluster. 0 to disable."
+    }),
 
-  max_metadata_per_user: yup.number().integer().meta({
-    description:
-      "The maximum number of active metrics with metadata for this tenant, per ingester. 0 to disable."
-  }),
-  max_metadata_per_metric: yup.number().integer().meta({
-    description:
-      "The maximum number of metadata per metric, per ingester. 0 to disable."
-  }),
-  max_global_metadata_per_user: yup.number().integer().meta({
-    description:
-      "The maximum number of active metrics with metadata for this tenant, across the cluster. 0 to disable."
-  }),
-  max_global_metadata_per_metric: yup.number().integer().meta({
-    description:
-      "The maximum number of metadata per metric, across the cluster. 0 to disable."
-  }),
+  max_metadata_per_user: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "The maximum number of active metrics with metadata for this tenant, per ingester. 0 to disable."
+    }),
+  max_metadata_per_metric: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "The maximum number of metadata per metric, per ingester. 0 to disable."
+    }),
+  max_global_metadata_per_user: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "The maximum number of active metrics with metadata for this tenant, across the cluster. 0 to disable."
+    }),
+  max_global_metadata_per_metric: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "The maximum number of metadata per metric, across the cluster. 0 to disable."
+    }),
 
-  max_fetched_chunks_per_query: yup.number().integer().meta({
-    description:
-      "Maximum number of chunks that can be fetched in a single query from ingesters and long-term storage: the total number of actual fetched chunks could be 2x the limit, being independently applied when querying ingesters and long-term storage. This limit is enforced in the ingester (if chunks streaming is enabled), querier, ruler and store-gateway."
-  }),
+  max_fetched_chunks_per_query: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "Maximum number of chunks that can be fetched in a single query from ingesters and long-term storage: the total number of actual fetched chunks could be 2x the limit, being independently applied when querying ingesters and long-term storage. This limit is enforced in the ingester (if chunks streaming is enabled), querier, ruler and store-gateway."
+    }),
   max_query_lookback: yup
     .string()
     .meta({
@@ -145,10 +197,14 @@ export const cortexLimitsSchema = yup.object({
         "Limit the query time range (end - start time). This limit is enforced in the query-frontend (on the received query), in the querier (on the query possibly split by the query-frontend) and in the chunks storage. 0 to disable."
     })
     .test(isDuration),
-  max_query_parallelism: yup.number().integer().meta({
-    description:
-      "Maximum number of split queries will be scheduled in parallel by the frontend."
-  }),
+  max_query_parallelism: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "Maximum number of split queries will be scheduled in parallel by the frontend."
+    }),
   max_cache_freshness: yup
     .string()
     .meta({
@@ -156,10 +212,14 @@ export const cortexLimitsSchema = yup.object({
         "Most recent allowed cacheable result for this tenant, to prevent caching very recent results that might still be in flux."
     })
     .test(isDuration),
-  max_queriers_per_tenant: yup.number().integer().meta({
-    description:
-      "Maximum number of queriers that can handle requests for this tenant. If set to 0 or value higher than number of available queriers, *all* queriers will handle requests for the tenant. Each frontend (or query-scheduler, if used) will select the same set of queriers for the same tenant (given that all queriers are connected to all frontends / query-schedulers). This option only works with queriers connecting to the query-frontend / query-scheduler, not when using downstream URL."
-  }),
+  max_queriers_per_tenant: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "Maximum number of queriers that can handle requests for this tenant. If set to 0 or value higher than number of available queriers, *all* queriers will handle requests for the tenant. Each frontend (or query-scheduler, if used) will select the same set of queriers for the same tenant (given that all queriers are connected to all frontends / query-schedulers). This option only works with queriers connecting to the query-frontend / query-scheduler, not when using downstream URL."
+    }),
   ruler_evaluation_delay_duration: yup
     .string()
     .meta({
@@ -167,21 +227,38 @@ export const cortexLimitsSchema = yup.object({
         "Duration to delay the evaluation of rules to ensure the underlying metrics have been pushed to Cortex."
     })
     .test(isDuration),
-  ruler_tenant_shard_size: yup.number().integer().meta({
-    description:
-      "This tenant's shard size. A value of 0 disables shuffle sharding for the Ruler in this tenant."
-  }),
-  ruler_max_rules_per_rule_group: yup.number().integer().meta({
-    description:
-      "Maximum number of rules per rule group for this tenant. 0 to disable."
-  }),
-  ruler_max_rule_groups_per_tenant: yup.number().integer().meta({
-    description: "Maximum number of rule groups for this tenant. 0 to disable."
-  }),
-  store_gateway_tenant_shard_size: yup.number().integer().meta({
-    description:
-      "This tenant's shard size. A value of 0 disables shuffle sharding for the tenant."
-  }),
+  ruler_tenant_shard_size: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "This tenant's shard size. A value of 0 disables shuffle sharding for the Ruler in this tenant."
+    }),
+  ruler_max_rules_per_rule_group: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "Maximum number of rules per rule group for this tenant. 0 to disable."
+    }),
+  ruler_max_rule_groups_per_tenant: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "Maximum number of rule groups for this tenant. 0 to disable."
+    }),
+  store_gateway_tenant_shard_size: yup
+    .number()
+    .integer()
+    .min(0, "Cannot be negative")
+    .meta({
+      description:
+        "This tenant's shard size. A value of 0 disables shuffle sharding for the tenant."
+    }),
   compactor_blocks_retention_period: yup
     .string()
     .meta({
@@ -192,6 +269,9 @@ export const cortexLimitsSchema = yup.object({
 });
 
 export type CortexLimits = yup.InferType<typeof cortexLimitsSchema>;
+export const CortexLimitsSchemaDescription = cortexLimitsSchema.fields;
+export type CortexLimitsKeys = keyof typeof CortexLimitsSchemaDescription;
+export type CortexLimitsValues = typeof CortexLimitsSchemaDescription[CortexLimitsKeys];
 
 export type RuntimeConfig = {
   overrides: {
