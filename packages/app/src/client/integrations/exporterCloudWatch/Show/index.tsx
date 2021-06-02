c66bcba251fb66974b2f5e3124bcc1833bd5d0e3
Nahum Wild <nahum@opstrace.com>
2021-06-03 09:37:46 +1200

    ui: update to reflect exporter integration pod names had change
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- packages/app/src/client/integrations/exporterCloudWatch/Show/index.tsx
+++ packages/app/src/client/integrations/exporterCloudWatch/Show/index.tsx
@@ -41,13 +41,13 @@ export const ExporterCloudWatchShow = () => {
 
   // NOTE: the timeranges for these urls is not the same as that used by the status component
   // const errorLogsUrl = useMemo(() => {
-  //   const path = `orgId=1&left=%5B%22now-1h%22,%22now%22,%22logs%22,%7B%22expr%22:%22%7Bk8s_namespace_name%3D%5C%22${tenant.name}-tenant%5C%22,k8s_container_name%3D%5C%22exporter%5C%22,k8s_pod_name%3D~%5C%22%5Eexporter-${integration.key}-%5Ba-z0-9-%5D*%5C%22%7D%20%7C%3D%20%5C%22stderr%5C%22%20%7C%3D%20%5C%22${ERROR_STR}%5C%22%22%7D%5D`;
+  //   const path = `orgId=1&left=%5B%22now-1h%22,%22now%22,%22logs%22,%7B%22expr%22:%22%7Bk8s_namespace_name%3D%5C%22${tenant.name}-tenant%5C%22,k8s_container_name%3D%5C%22exporter%5C%22,k8s_pod_name%3D~%5C%22%5Eintegration-${integration.key}-%5Ba-z0-9-%5D*%5C%22%7D%20%7C%3D%20%5C%22stderr%5C%22%20%7C%3D%20%5C%22${ERROR_STR}%5C%22%22%7D%5D`;
   //   return `${window.location.protocol}//system.${window.location.host}/grafana/explore?${path}`;
   // }, [tenant.name, integration.key]);
 
   const logsUrl = useMemo(() => {
     if (integration?.key) {
-      const path = `orgId=1&left=%5B%22now-1h%22,%22now%22,%22logs%22,%7B%22expr%22:%22%7Bk8s_namespace_name%3D%5C%22${tenant.name}-tenant%5C%22,k8s_container_name%3D%5C%22exporter%5C%22,k8s_pod_name%3D~%5C%22%5Eexporter-${integration.key}-%5Ba-z0-9-%5D*%5C%22%7D%22%7D%5D`;
+      const path = `orgId=1&left=%5B%22now-1h%22,%22now%22,%22logs%22,%7B%22expr%22:%22%7Bk8s_namespace_name%3D%5C%22${tenant.name}-tenant%5C%22,k8s_container_name%3D%5C%22exporter%5C%22,k8s_pod_name%3D~%5C%22%5Eintegration-${integration.key}-%5Ba-z0-9-%5D*%5C%22%7D%22%7D%5D`;
       return `${window.location.protocol}//system.${window.location.host}/grafana/explore?${path}`;
     } else return "";
   }, [tenant.name, integration?.key]);
