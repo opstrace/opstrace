f72728698af98c34e75ff854bf8a80df1100fe34
Nahum Wild <nahum@opstrace.com>
2021-06-14 09:30:07 +1200

    ui: explictly state tests are "headless" as the lauch error might be around trying to run headed
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- test/test-remote/playwright.config.ts
+++ test/test-remote/playwright.config.ts
@@ -18,6 +18,7 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   use: {
+    headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     screenshot: "only-on-failure",
