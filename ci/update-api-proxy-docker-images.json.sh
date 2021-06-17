c440177a0ce65fc38d8596698d357b416d56fe06
Simão Reis <sreis@opstrace.com>
2021-06-17 11:15:18 +0000

    makefile: fix update docker images script
    Account for app, graphql images.
    
    Signed-off-by: Simão Reis <sreis@opstrace.com>

--- ci/update-api-proxy-docker-images.json.sh
+++ ci/update-api-proxy-docker-images.json.sh
@@ -7,23 +7,24 @@ set -o pipefail
 # check if docker-images.json has the required docker images set
 CORTEX_API_PROXY_IMAGE=$(cd ${DIR}/../go/ && make -s DOCKER_IMAGE_NAME=cortex-api print-docker-image-name-tag)
 LOKI_API_PROXY_IMAGE=$(cd ${DIR}/../go/ && make -s DOCKER_IMAGE_NAME=loki-api print-docker-image-name-tag)
+DD_API_PROXY_IMAGE=$(cd ${DIR}/../go/ && make -s DOCKER_IMAGE_NAME=ddapi print-docker-image-name-tag)
+CONFIG_API_PROXY_IMAGE=$(cd ${DIR}/../go/ && make -s DOCKER_IMAGE_NAME=config-api print-docker-image-name-tag)
+OPSTRACE_APP_IMAGE=$(cd ${DIR}/../packages/app/ && make -s DOCKER_IMAGE_NAME=app print-docker-image-name-tag)
+OPSTRACE_GRAPHQL_IMAGE=$(cd ${DIR}/../packages/app/ && make -s DOCKER_IMAGE_NAME=graphql print-docker-image-name-tag)
 
-(jq -e '.cortexApiProxy == "'${CORTEX_API_PROXY_IMAGE}'"' ${DIR}/../packages/controller-config/src/docker-images.json &&
-jq -e '.lokiApiProxy == "'${LOKI_API_PROXY_IMAGE}'"' ${DIR}/../packages/controller-config/src/docker-images.json) > /dev/null
-
-EXIT_CODE=$?
-
-if [ $EXIT_CODE -eq 0 ];
-then
-	echo "No changes required to docker-images.json."
-fi
-
+# wrapper function to edit json file inplace
 jqi() {
 	cat <<< "$(jq "$1" < "$2")" > "$2"
 }
 
 # update docker-images.json
-jqi '.cortexApiProxy = "'${CORTEX_API_PROXY_IMAGE}'"' ${DIR}/../packages/controller-config/src/docker-images.json
-jqi '.lokiApiProxy = "'${LOKI_API_PROXY_IMAGE}'"' ${DIR}/../packages/controller-config/src/docker-images.json
+DOCKER_IMAGES_JSON=${DIR}/../packages/controller-config/src/docker-images.json
+
+jqi '.cortexApiProxy = "'${CORTEX_API_PROXY_IMAGE}'"' ${DOCKER_IMAGES_JSON}
+jqi '.lokiApiProxy = "'${LOKI_API_PROXY_IMAGE}'"' ${DOCKER_IMAGES_JSON}
+jqi '.ddApi = "'${DD_API_PROXY_IMAGE}'"' ${DOCKER_IMAGES_JSON}
+jqi '.configApi = "'${CONFIG_API_PROXY_IMAGE}'"' ${DOCKER_IMAGES_JSON}
+jqi '.app = "'${OPSTRACE_APP_IMAGE}'"' ${DOCKER_IMAGES_JSON}
+jqi '.graphqlEngine = "'${OPSTRACE_GRAPHQL_IMAGE}'"' ${DOCKER_IMAGES_JSON}
 
 echo "Please add a commit with the required changes to docker-images.json."
