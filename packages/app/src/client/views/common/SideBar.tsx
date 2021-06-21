06c2d2ab87b0687714ca0bfaa91a153666ed1bab
Nahum Wild <nahum@opstrace.com>
2021-06-21 15:47:30 +1200

    ui: include "data-pw=<item>" data attribute on sidebar for playwright to try hovering over
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- packages/app/src/client/views/common/SideBar.tsx
+++ packages/app/src/client/views/common/SideBar.tsx
@@ -104,6 +104,7 @@ const NavItemContents = ({
     <ListItem
       dense
       onClick={onClick}
+      data-pw={`sidebar/${item.title}`}
       className={className({
         [classes.activeItem]: routeMatch,
         [classes.listItem]: true,
