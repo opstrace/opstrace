2dfbb51e975d5cc15d1d9fc13fbf739adc7f3475
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-06-28 17:30:51 +0200

    chore: .eslintrc.js: breaking change
    https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md
    version-800-2021-02-21
    
    Error: Cannot read config file: \
    /build/node_modules/eslint-config-prettier/@typescript-eslint.js
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint"
+    "prettier"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
