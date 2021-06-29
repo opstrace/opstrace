c76a325da0b1a2e4ca0ab476be313d4fc5b28af1
Nahum Wild <nahum@opstrace.com>
2021-06-30 07:44:25 +1200

    ui: mark the everywhere browser test as "slow", this tripples it's allowable time
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- test/browser/tests/everywhere.spec.ts
+++ test/browser/tests/everywhere.spec.ts
@@ -22,64 +22,66 @@ import { logUserIn } from "../utils/authentication";
 test.describe("after auth0 authentication", () => {
   test.beforeEach(logUserIn);
 
-  test.only("user should click around the entire site", async ({
-    page,
-    user
-  }) => {
-    expect(await page.isVisible("[data-test=getting-started]")).toBeTruthy();
+  test.slow(
+    "user should click around the entire site",
+    async ({ page, user }) => {
+      expect(await page.isVisible("[data-test=getting-started]")).toBeTruthy();
 
-    await page.hover("[data-test='sidebar/tenant/Overview']");
-    await page.click("[data-test='sidebar/tenant/Overview']");
-    await page.click("[data-test='sidebar/tenant/Overview/Metrics']");
-    await page.click("[data-test='sidebar/tenant/Overview/Logs']");
-    await page.click("[data-test='sidebar/tenant/Overview']");
+      await page.hover("[data-test='sidebar/tenant/Overview']");
+      await page.click("[data-test='sidebar/tenant/Overview']");
+      await page.click("[data-test='sidebar/tenant/Overview/Metrics']");
+      await page.click("[data-test='sidebar/tenant/Overview/Logs']");
+      await page.click("[data-test='sidebar/tenant/Overview']");
 
-    await page.hover("[data-test='sidebar/tenant/Dashboards']");
-    await page.click("[data-test='sidebar/tenant/Dashboards']");
+      await page.hover("[data-test='sidebar/tenant/Dashboards']");
+      await page.click("[data-test='sidebar/tenant/Dashboards']");
 
-    await page.hover("[data-test='sidebar/tenant/Explore']");
-    await page.click("[data-test='sidebar/tenant/Explore']");
+      await page.hover("[data-test='sidebar/tenant/Explore']");
+      await page.click("[data-test='sidebar/tenant/Explore']");
 
-    await page.hover("[data-test='sidebar/tenant/Alerting']");
-    await page.click("[data-test='sidebar/tenant/Alerting']");
+      await page.hover("[data-test='sidebar/tenant/Alerting']");
+      await page.click("[data-test='sidebar/tenant/Alerting']");
 
-    await page.hover("[data-test='sidebar/tenant/Users']");
-    await page.click("[data-test='sidebar/tenant/Users']");
-    expect(
-      await page.isVisible(`[data-test='userList/${user.email}']`)
-    ).toBeTruthy();
-    await page.click("[data-test='sidebar/tenant/Users']");
+      await page.hover("[data-test='sidebar/tenant/Users']");
+      await page.click("[data-test='sidebar/tenant/Users']");
+      expect(
+        await page.isVisible(`[data-test='userList/${user.email}']`)
+      ).toBeTruthy();
+      await page.click("[data-test='sidebar/tenant/Users']");
 
-    await page.hover("[data-test='sidebar/tenant/Integrations']");
-    await page.click("[data-test='sidebar/tenant/Integrations']");
-    await page.click("[data-test='sidebar/tenant/Integrations/All']");
-    expect(
-      await page.isVisible(
-        `[data-test='integrations/grid/exporter-cloud-monitoring']`
-      )
-    ).toBeTruthy();
-    await page.click("[data-test='sidebar/tenant/Integrations']");
+      await page.hover("[data-test='sidebar/tenant/Integrations']");
+      await page.click("[data-test='sidebar/tenant/Integrations']");
+      await page.click("[data-test='sidebar/tenant/Integrations/All']");
+      expect(
+        await page.isVisible(
+          `[data-test='integrations/grid/exporter-cloud-monitoring']`
+        )
+      ).toBeTruthy();
+      await page.click("[data-test='sidebar/tenant/Integrations']");
 
-    await page.hover("[data-test='sidebar/clusterAdmin/Health']");
-    await page.click("[data-test='sidebar/clusterAdmin/Health']");
-    await page.click("[data-test='sidebar/clusterAdmin/Health/System']");
-    await page.click("[data-test='sidebar/clusterAdmin/Health/Metrics']");
-    await page.click("[data-test='sidebar/clusterAdmin/Health/Logs']");
-    await page.click("[data-test='sidebar/clusterAdmin/Health']");
+      await page.hover("[data-test='sidebar/clusterAdmin/Health']");
+      await page.click("[data-test='sidebar/clusterAdmin/Health']");
+      await page.click("[data-test='sidebar/clusterAdmin/Health/System']");
+      await page.click("[data-test='sidebar/clusterAdmin/Health/Metrics']");
+      await page.click("[data-test='sidebar/clusterAdmin/Health/Logs']");
+      await page.click("[data-test='sidebar/clusterAdmin/Health']");
 
-    await page.hover("[data-test='sidebar/clusterAdmin/Users']");
-    await page.click("[data-test='sidebar/clusterAdmin/Users']");
-    expect(
-      await page.isVisible(`[data-test='userList/${user.email}']`)
-    ).toBeTruthy();
-    await page.click("[data-test='sidebar/clusterAdmin/Users']");
+      await page.hover("[data-test='sidebar/clusterAdmin/Users']");
+      await page.click("[data-test='sidebar/clusterAdmin/Users']");
+      expect(
+        await page.isVisible(`[data-test='userList/${user.email}']`)
+      ).toBeTruthy();
+      await page.click("[data-test='sidebar/clusterAdmin/Users']");
 
-    await page.hover("[data-test='sidebar/clusterAdmin/Tenants']");
-    await page.click("[data-test='sidebar/clusterAdmin/Tenants']");
+      await page.hover("[data-test='sidebar/clusterAdmin/Tenants']");
+      await page.click("[data-test='sidebar/clusterAdmin/Tenants']");
 
-    await page.hover("[data-test='sidebar/clusterAdmin/Configuration']");
-    await page.click("[data-test='sidebar/clusterAdmin/Configuration']");
-    await page.click("[data-test='sidebar/clusterAdmin/Configuration/Cortex']");
-    await page.hover("[data-test='sidebar/clusterAdmin/Configuration']");
-  });
+      await page.hover("[data-test='sidebar/clusterAdmin/Configuration']");
+      await page.click("[data-test='sidebar/clusterAdmin/Configuration']");
+      await page.click(
+        "[data-test='sidebar/clusterAdmin/Configuration/Cortex']"
+      );
+      await page.hover("[data-test='sidebar/clusterAdmin/Configuration']");
+    }
+  );
 });
