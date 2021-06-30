2d528b7c551a8b7b410daff0271699708c867d6b
Simão Reis <sreis@opstrace.com>
2021-06-30 13:36:48 +0000

    ci: fix target rename
    Signed-off-by: Simão Reis <sreis@opstrace.com>

--- ci/test-upgrade/preamble.sh
+++ ci/test-upgrade/preamble.sh
@@ -4,4 +4,4 @@ make rebuild-ci-container-image
 # testrunner run tsc which requires buildinfo package to be set
 make set-build-info-constants
 make rebuild-testrunner-container-images
-make rebuild-looker-container-images
+make rebuild-looker-container-image
