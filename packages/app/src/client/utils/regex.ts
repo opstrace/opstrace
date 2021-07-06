50278550b6a851e1a14762cfcc715282899e278f
Nahum Wild <nahum@opstrace.com>
2021-07-07 11:47:51 +1200

    ui: simplified tenant name validation regex as the only valid chars are alphanumeric now
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- packages/app/src/client/utils/regex.ts
+++ packages/app/src/client/utils/regex.ts
@@ -17,6 +17,7 @@
 // note: webkit doesn't support "look behind" syntax which means can't use the preferred regex for this
 // const tenantNameValidatorWithLookBehind = /^(?!-)[a-z0-9-]{1,63}(?<!-)$/;
 
-// 2021.07.06 - [terrcin] I've removed "-" as a valid char as we're having problems with it in Grafana pod names
-//              related issue: https://github.com/opstrace/opstrace/issues/957
-export const tenantNameValidator = /^[a-z0-9][a-z0-9]{0,61}[a-z0-9]$/;
+// 2021.07.06 - [terrcin] I've removed "-" as a valid char as we're having problems with it in Grafana
+// pod names. This means that only alphanumeric chars are allowed.
+// Related issue: https://github.com/opstrace/opstrace/issues/957
+export const tenantNameValidator = /^[a-z0-9]{1,63}$/;
