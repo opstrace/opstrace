96f6e8e41c8042af84e4446bb5f6655e0ad7a8bf
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-07-14 13:50:12 +0200

    chore: createOrUpdateConfigMapWithRetry: add forceCreate opt
    Also rework error handling to account for
    more transients w/o HTTP response (not
    just e.code === ETIMEOUT) and account for
    all 5xx responses.
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- lib/kubernetes/src/index.ts
+++ lib/kubernetes/src/index.ts
@@ -2,7 +2,16 @@
  * Copyright 2020 Opstrace, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+
+      // Handle transient/retryable errors here, mainly TCP connect() timeout
+      // For context, see opstrace-prelaunch/issues/1039 and
+      // https://github.com/kubernetes-client/javascript/issues/544
+      // TODO: handle other transient ones, ENOUTFOUND and 5xx HTTP responses.
+      if (e.code && e.code === "ETIMEDOUT") {
+        log.info("retry config map creation/update soon (%s)", e.message);
+        await sleep(3);
+        continue;
+      }* you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
@@ -37,12 +46,25 @@ import { log, sleep, mtime, mtimeDeadlineInSeconds } from "@opstrace/utils";
  * @param cm
  * @param forceUpdate: skip initial CREATE attempt. Expected to fail when the
  *  config map does not exist yet.
+ * @param forceCreate: error out when seeing a 409 response indicating that the
+ * config map already exists (re-throw 409 error in this case)
  */
 
 export async function createOrUpdateConfigMapWithRetry(
   cm: ConfigMap,
-  forceUpdate = false
+  opts: {
+    forceUpdate?: boolean;
+    forceCreate?: boolean;
+  } = {
+    forceUpdate: false,
+    forceCreate: false
+  }
 ): Promise<void> {
+  if (opts.forceUpdate && opts.forceCreate) {
+    // programmer error
+    throw new Error("must not set both, forceCreate and forceUpdate");
+  }
+
   // For choosing the overall operation deadline note that the connect()
   // timeout control for the underlying HTTP client is not yet working well --
   // might take ~2 minutes, see
@@ -79,26 +101,85 @@ export async function createOrUpdateConfigMapWithRetry(
     }
 
     try {
-      if (forceUpdate || seen409) {
+      if (opts.forceUpdate || seen409) {
+        // fallback from create to update (if `forceCreate` is `false` )
         await update(cycle);
       } else {
         await create(cycle);
       }
     } catch (e) {
-      if (e.response && e.response.statusCode === 409) {
+      if (e.response === undefined) {
+        // Handle transient/retryable errors here, mainly TCP connect() timeout
+        // For context, see opstrace-prelaunch/issues/1039 and
+        // https://github.com/kubernetes-client/javascript/issues/544
+        // and also other transient ones, ENOUTFOUND
+        if (e.code) {
+          // for example: e.code === "ETIMEDOUT"
+          log.info("retry config map creation/update soon (%s)", e.message);
+          await sleep(4);
+          continue;
+        }
+      }
+
+      // got an HTTP response.
+      const statuscode = e.response.statusCode as number;
+
+      // Expect this error response structure:
+      // "response": {
+      //   "statusCode": 404,
+      //   "body": {
+      //     "kind": "Status",
+      //     "apiVersion": "v1",
+      //     "metadata": {},
+      //     "status": "Failure",
+      //     "message": "configmaps \"opstrace-controller-config\" not found",
+      //     "reason": "NotFound",
+      //     "details": {
+      //       "name": "opstrace-controller-config",
+      //       "kind": "configmaps"
+      //     },
+      //     "code": 404
+      //   },
+      //...
+
+      // handle 5xx responses, treat all of them as retryable.
+      if (statuscode.toString().startsWith("5")) {
+        log.debug("e.response: %s", JSON.stringify(e.response, null, 2));
         log.info(
-          "409 response: config map already exists, try updating instead"
+          "retry config map creation/update soon as of 5xx response (%s)",
+          e.message
         );
-        seen409 = true;
+        await sleep(5);
         continue;
       }
 
-      // Handle transient/retryable errors here, mainly TCP connect() timeout
-      // For context, see opstrace-prelaunch/issues/1039 and
-      // https://github.com/kubernetes-client/javascript/issues/544
-      if (e.code && e.code === "ETIMEDOUT") {
-        log.info("retry config map creation/update soon (%s)", e.message);
-        await sleep(3);
+      if (statuscode === 404) {
+        // I think a 404 response means that either PATCH was called (for
+        // updating the config map) w/o the config map existing in advance, or
+        // also when PUT is called (for creating the config map) when e.g. the
+        // namespace does not exist yet. I think in any case we do not want to
+        // retry this.
+
+        if (opts.forceUpdate) {
+          log.warning("got 404 response, and forceUpdate is set");
+        }
+
+        // Re-throw error to let caller see the same 404 error detail.
+        throw e;
+      }
+
+      if (statuscode === 409) {
+        if (opts.forceCreate) {
+          log.warning("config map already exists, but forceCreate is set");
+          // Re-throw error to let caller see the same 409 error detail.
+          throw e;
+        }
+
+        log.info(
+          "409 response: config map already exists, try updating instead"
+        );
+        // This means we fall back from create to update:
+        seen409 = true;
         continue;
       }
 
