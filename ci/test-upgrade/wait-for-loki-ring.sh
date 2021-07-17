40d233f27d796bda48f2143a480a914f868df50c
Simão Reis <sreis@opstrace.com>
2021-07-17 10:28:41 +0000

    ci(upgrades): check ring status in all loki distributor pods
    It takes a while for the membership ring status to propagate to
    all distributors so we need to wait until all the pods report
    a correct ring status.
    
    Also, GCP does not seem to like 'kubectl get raw' so we need to
    set up port forwarding to the distributor pods to be able to
    curl the endpoints.
    
    Signed-off-by: Simão Reis <sreis@opstrace.com>

--- ci/test-upgrade/wait-for-loki-ring.sh
+++ ci/test-upgrade/wait-for-loki-ring.sh
@@ -2,14 +2,31 @@
 
 echo "--- waiting for loki ring to be ready"
 
-for cycle in {1..30}
-do
-    SHARDS=$(kubectl get --raw /api/v1/namespaces/loki/services/distributor:1080/proxy/ring | jq .shards | jq length)
-    echo "number of shards in ring: ${SHARDS}"
-    [[ ${SHARDS} < 3 ]] || exit 0
+function check_distributor() {
+    local pod=${1}
+    echo "set up port-forward to pod ${pod}"
+    # Pick a random port in the given range.
+    local PORT=$(shuf -i 8080-9000 -n 1)
+    # This process is killed when the container running this script exits.
+    kubectl --namespace loki port-forward pod/${pod} ${PORT}:1080 &
+    echo "waiting for pod ${pod} to report ring is ready"
+    for cycle in {1..20}
+    do
+        # The initial sleep here waits for the port forwarding to be set up.
+        sleep 30
+        SHARDS=$(curl -m 20 -s -H "Accept: application/json, */*"  http://localhost:${PORT}/ring | jq .shards | jq length)
+        echo "pod ${pod}: reports number of shards in ring: ${SHARDS}"
 
-    sleep 30
+        (( ${SHARDS} >= 3 )) && break
+    done
+}
+
+DISTRIBUTORS=$(kubectl --namespace loki get pods --no-headers -o custom-columns=":metadata.name" | grep distributor)
+
+for pod in ${DISTRIBUTORS}
+do
+    check_distributor ${pod}
+    (( ${SHARDS} < 3 )) && echo "timeout waiting for loki ring to be ready" && exit 1
 done
 
-echo "timeout waiting for loki ring to be ready"
-exit 1
+exit 0
