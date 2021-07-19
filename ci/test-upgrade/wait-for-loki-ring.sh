aedc40b096046a8419d8ad88c18376e69e93374e
Simão Reis <sreis@opstrace.com>
2021-07-19 12:28:00 +0000

    cli(upgrade): restart loki distributors
    Workaround for opstrace/opstrace#1066 to force distributors to
    find ingesters. Otherwise, it might take a long time for the
    distributors to forget the old ingesters and find the new ones.
    
    Signed-off-by: Simão Reis <sreis@opstrace.com>

--- ci/test-upgrade/wait-for-loki-ring.sh
+++ ci/test-upgrade/wait-for-loki-ring.sh
@@ -9,20 +9,21 @@ function check_distributor() {
     local PORT=$(shuf -i 8080-9000 -n 1)
     # This process is killed when the container running this script exits.
     kubectl --namespace loki port-forward pod/${pod} ${PORT}:1080 &
+    # Wait for the port forwarding to be set up.
+    sleep 5
+
     echo "waiting for pod ${pod} to report ring is ready"
     for cycle in {1..20}
     do
-        # The initial sleep here waits for the port forwarding to be set up.
-        sleep 30
         SHARDS=$(curl -m 20 -s -H "Accept: application/json, */*"  http://localhost:${PORT}/ring | jq .shards | jq length)
         echo "pod ${pod}: reports number of shards in ring: ${SHARDS}"
 
         (( ${SHARDS} >= 3 )) && break
+        sleep 30
     done
 }
 
 DISTRIBUTORS=$(kubectl --namespace loki get pods --no-headers -o custom-columns=":metadata.name" | grep distributor)
-
 for pod in ${DISTRIBUTORS}
 do
     check_distributor ${pod}
