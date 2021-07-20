08c3968c749b52476686c346035a0e141309a9b7
Simão Reis <sreis@opstrace.com>
2021-07-20 15:12:49 +0000

    ci: run upgrade tests on GCP as part of PR pipeline
    Signed-off-by: Simão Reis <sreis@opstrace.com>

--- ci/test-upgrade/fetch-cli-artifacts.sh
+++ ci/test-upgrade/fetch-cli-artifacts.sh
@@ -18,16 +18,25 @@ fi
 # Funtion that downloads cli artifact from s3 bucket and extracts it to a target
 # dir.
 #
-fetch_cli_artifact() {
-    local artifact=${1}
-    local dir=${2}
-
+fetch_cli_s3_artifact() {
     echo "downloading ${artifact}"
-    mkdir -p ${dir}
     aws s3 cp --only-show-errors s3://opstrace-ci-main-artifacts/${artifact} .
 
     echo "extracting ${artifact} to target dir ${dir}"
     tar xjf $(basename ${artifact}) -C ${dir}
+}
+
+fetch_cli_artifact() {
+    local artifact=${1}
+    local dir=${2}
+
+    mkdir -p ${dir}
+
+    if [ -f "${artifact}" ]; then
+        cp ${artifact} ${dir}
+    else
+        fetch_cli_s3_artifact ${artifact} ${dir}
+    fi
 }
 
 echo "--- fetching cli artifacts"
