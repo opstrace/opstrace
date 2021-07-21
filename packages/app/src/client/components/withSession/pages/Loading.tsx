f3d6868a0caeab0b6c819b74d1f66c70dec614fc
Nahum Wild <nahum@opstrace.com>
2021-07-21 15:20:35 +1200

    ui: custom images on loading page depending on the stage of the process the user is in
    Signed-off-by: Nahum Wild <nahum@opstrace.com>

--- packages/app/src/client/components/withSession/pages/Loading.tsx
+++ packages/app/src/client/components/withSession/pages/Loading.tsx
@@ -16,6 +16,39 @@
 
 import React from "react";
 
-import Loading from "client/components/Loadable/Loading";
+import { Page } from "client/components/Page";
+import { Box } from "client/components/Box";
+import { Typography } from "client/components/Typography";
 
-export const LoadingPage = () => <Loading />;
+import accessDeniedSvg from "./loadingStages/tracy-status-check.svg";
+import auth0Svg from "./loadingStages/tracy-auth0.svg";
+import createSessionSvg from "./loadingStages/tracy-create-session.svg";
+import errorSvg from "./loadingStages/tracy-error.svg";
+import logoutSvg from "./loadingStages/tracy-logout.svg";
+import statusCheckSvg from "./loadingStages/tracy-status-check.svg";
+
+const STAGES: Record<string, any> = {
+  "access-denied": accessDeniedSvg,
+  auth0: auth0Svg,
+  "create-session": createSessionSvg,
+  error: errorSvg,
+  logout: logoutSvg,
+  "status-check": statusCheckSvg
+};
+
+type LoadingPageProps = {
+  stage: string;
+};
+
+export const LoadingPage = ({ stage }: LoadingPageProps) => (
+  <Page centered height="100vh" width="100vw">
+    <Box p={1} mb={4} display="flex" width="100%" justifyContent="center">
+      <Box p={1} height={150} width={100}>
+        <img src={STAGES[stage]} alt="" />
+      </Box>
+      <Box p={1} height={150} display="flex" alignItems="center">
+        <Typography variant="h3">opstrace</Typography>
+      </Box>
+    </Box>
+  </Page>
+);
