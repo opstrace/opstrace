bf8769e4d49b84254ef236e003aff9f84bff98ae
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-07-26 10:25:50 +0200

    test-remote: metrics.ts: auto-format
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- test/test-remote/testutils/metrics.ts
+++ test/test-remote/testutils/metrics.ts
@@ -14,11 +14,7 @@
  * limitations under the License.
  */
 
-import {
-  log,
-  queryJSONAPI,
-  waitForQueryResult
-} from "./index";
+import { log, queryJSONAPI, waitForQueryResult } from "./index";
 
 import { PortForward } from "./portforward";
 
@@ -45,16 +41,16 @@ export async function waitForCortexMetricResult(
 
   return waitForQueryResult(
     () => queryJSONAPI(url, qparms),
-    (data) => {
+    data => {
       // Example data:
       // - query: https://prometheus.io/docs/prometheus/latest/querying/api/#instant-queries
       // - query_range: https://prometheus.io/docs/prometheus/latest/querying/api/#range-queries
       // Metrics data has results nested here, wait for non-empty result:
       const resultArray = data["data"]["result"];
-      return (resultArray.length > 0) ? resultArray : null;
+      return resultArray.length > 0 ? resultArray : null;
     },
     maxWaitSeconds,
-    logQueryResponse,
+    logQueryResponse
   );
 }
 
@@ -64,28 +60,32 @@ export async function waitForPrometheusTarget(
   jobLabel: string,
   // Use a long timeout when waiting for prometheus scraper to see the pod.
   // Normally takes 5-15s, but can take longer than 30s
-  maxWaitSeconds = 300,
+  maxWaitSeconds = 300
 ) {
   const portForwardProm = new PortForward(
     `tenant-prometheus-${tenant}`, // name (arbitrary/logging)
     `statefulsets/prometheus-${tenant}-prometheus`, // k8sobj
     9090, // port_remote
-    `${tenant}-tenant`, // namespace
+    `${tenant}-tenant` // namespace
   );
   const localPort = await portForwardProm.setup();
 
   try {
     const url = `http://127.0.0.1:${localPort}/prometheus/api/v1/targets`;
-    const qparms = new URLSearchParams({state: "active"});
+    const qparms = new URLSearchParams({ state: "active" });
 
-    log.info(`Waiting for target with tenant=${tenant} job=${jobLabel} via port-forward: ${url}`)
+    log.info(
+      `Waiting for target with tenant=${tenant} job=${jobLabel} via port-forward: ${url}`
+    );
     await waitForQueryResult(
       () => queryJSONAPI(url, qparms),
-      (data) => {
+      data => {
         // Example data: https://prometheus.io/docs/prometheus/latest/querying/api/#targets
         // Search for target(s) with matching job label
         const targets: Array<any> = data["data"]["activeTargets"];
-        const filtered = targets.filter(target => target["labels"]["job"] === jobLabel);
+        const filtered = targets.filter(
+          target => target["labels"]["job"] === jobLabel
+        );
         if (filtered.length == 0) {
           // Not found yet
           return null;
@@ -94,13 +94,16 @@ export async function waitForPrometheusTarget(
           "Found tenant prometheus scrape targets for job=%s: %s",
           jobLabel,
           filtered
-            .map(t => `${t["labels"]["job"]}:${t["labels"]["namespace"]}/${t["labels"]["pod"]}`)
+            .map(
+              t =>
+                `${t["labels"]["job"]}:${t["labels"]["namespace"]}/${t["labels"]["pod"]}`
+            )
             .sort()
         );
         return filtered;
       },
       maxWaitSeconds,
-      false, // This is VERY long for system tenant, so don't log
+      false // This is VERY long for system tenant, so don't log
     );
   } finally {
     await portForwardProm.terminate();
