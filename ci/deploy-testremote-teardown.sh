97bcd3aed1dd65c8757f7a2c9ca5b821d492d5c1
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-08-09 14:01:52 +0200

    ci: remove gcp projects d+f, add a+b+c
    Small sample result is that #293 is limited
    to ddd and fff.
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- ci/deploy-testremote-teardown.sh
+++ ci/deploy-testremote-teardown.sh
@@ -68,7 +68,8 @@ source secrets/opstrace_dockerhub_creds.sh
 # https://github.com/opstrace/opstrace/pull/128#issuecomment-742519078 and
 # https://stackoverflow.com/q/5189913/145400.
 #OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-aaa ci-shard-bbb ci-shard-ccc)
-OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-ddd ci-shard-eee ci-shard-fff)
+# remove eee and fff for now, see issue #293
+OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-aaa ci-shard-bbb ci-shard-ccc ci-shard-eee)
 echo "--- random choice for GCP project ID: ${OPSTRACE_GCP_PROJECT_ID}"
 export GOOGLE_APPLICATION_CREDENTIALS=./secrets/gcp-svc-acc-${OPSTRACE_GCP_PROJECT_ID}.json
 
--- ci/test-upgrade/run.sh
+++ ci/test-upgrade/run.sh
@@ -18,7 +18,8 @@ case "${OPSTRACE_CLOUD_PROVIDER}" in
     gcp)
         # Shard across GCP CI projects.
         #export OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-aaa ci-shard-bbb ci-shard-ccc)
-        export OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-ddd ci-shard-eee ci-shard-fff)
+        # see issue 293
+        export OPSTRACE_GCP_PROJECT_ID=$(shuf -n1 -e ci-shard-aaa ci-shard-bbb ci-shard-ccc ci-shard-eee)
         echo "--- random choice for GCP project ID: ${OPSTRACE_GCP_PROJECT_ID}"
         export GOOGLE_APPLICATION_CREDENTIALS=./secrets/gcp-svc-acc-${OPSTRACE_GCP_PROJECT_ID}.json
         ;;
