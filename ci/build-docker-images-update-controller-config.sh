fa1b71b0591c98335bc29943131e46fa1998bd84
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-08-11 14:46:37 +0200

    ci: build-docker-images....sh: generate new JSON early
    - Add commentary to document better what's happening
    - (Over)write the docker-images.json as early as
      the data is there, so that other processes can
      run concurrently and pick this new file up.
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- ci/build-docker-images-update-controller-config.sh
+++ ci/build-docker-images-update-controller-config.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
 
+# This is probably the absolute path to /ci in our rep
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 set -oux pipefail
 
+echo "DIR: ${DIR}"
+echo "pwd: $(pwd)"
+
+# edit JSON file in place
+jqi() {
+  cat <<< "$(jq "$1" < "$2")" > "$2"
+}
+
+# These image tag generation technqiues are all based on traversing a directory
+# tree and building hash over file contents. See corresponding Makefile(s).
 CORTEX_API_PROXY_IMAGE=$(cd ${DIR}/../go/ && make -s DOCKER_IMAGE_NAME=cortex-api print-docker-image-name-tag)
 LOKI_API_PROXY_IMAGE=$(cd ${DIR}/../go/ && make -s DOCKER_IMAGE_NAME=loki-api print-docker-image-name-tag)
 DD_API_PROXY_IMAGE=$(cd ${DIR}/../go/ && make -s DOCKER_IMAGE_NAME=ddapi print-docker-image-name-tag)
 CONFIG_API_PROXY_IMAGE=$(cd ${DIR}/../go/ && make -s DOCKER_IMAGE_NAME=config-api print-docker-image-name-tag)
+OPSTRACE_APP_IMAGE=$(cd ${DIR}/../packages/app/ && make -s DOCKER_IMAGE_NAME=app print-docker-image-name-tag)
+OPSTRACE_GRAPHQL_IMAGE=$(cd ${DIR}/../packages/app/ && make -s DOCKER_IMAGE_NAME=graphql print-docker-image-name-tag)
 
-echo "Check if api docker images exist"
+# Write a new version of docker-images.json super early in this 'script' so
+# that the controller image build (which needs an updated version of this file)
+# can happen concurrently.
+DIJSON_PATH="${DIR}/../packages/controller-config/src/docker-images.json"
+echo "Updating docker-images.json"
+jqi '.cortexApiProxy = "'${CORTEX_API_PROXY_IMAGE}'"' "${DIJSON_PATH}"
+jqi '.lokiApiProxy = "'${LOKI_API_PROXY_IMAGE}'"' "${DIJSON_PATH}"
+jqi '.ddApi = "'${DD_API_PROXY_IMAGE}'"' "${DIJSON_PATH}"
+jqi '.configApi = "'${CONFIG_API_PROXY_IMAGE}'"' "${DIJSON_PATH}"
+jqi '.app = "'${OPSTRACE_APP_IMAGE}'"' "${DIJSON_PATH}"
+jqi '.graphqlEngine = "'${OPSTRACE_GRAPHQL_IMAGE}'"' "${DIJSON_PATH}"
+
+echo "Updated docker images, this is the diff:"
+git --no-pager diff "${DIJSON_PATH}"
+
+# signal to the other racers that docker-images.json got updated. Note(JP):
+# it's probably better to make the docker-images.json in the repo to be
+# obviously 'wrong', maybe filled with template variables. Maybe merge this
+# with the set-build-info-constants main makefile target.
+touch ${DIR}/docker_images_json_regenerated
+
+echo "Check if Go-based / API docker images exist"
 docker pull ${CORTEX_API_PROXY_IMAGE} && docker pull ${LOKI_API_PROXY_IMAGE} && docker pull ${DD_API_PROXY_IMAGE} && docker pull ${CONFIG_API_PROXY_IMAGE}
-
 if [ $? -ne 0 ];
 then
 	set -e
@@ -23,12 +56,8 @@ then
 	(cd ${DIR}/../go && make publish)
 fi
 
-OPSTRACE_APP_IMAGE=$(cd ${DIR}/../packages/app/ && make -s DOCKER_IMAGE_NAME=app print-docker-image-name-tag)
-OPSTRACE_GRAPHQL_IMAGE=$(cd ${DIR}/../packages/app/ && make -s DOCKER_IMAGE_NAME=graphql print-docker-image-name-tag)
-
-echo "Check if app docker images exist"
+echo "Check if UI app docker images exist"
 docker pull ${OPSTRACE_APP_IMAGE} && docker pull ${OPSTRACE_GRAPHQL_IMAGE}
-
 if [ $? -ne 0 ];
 then
 	set -e
@@ -41,26 +70,3 @@ then
 fi
 
 set -e
-
-#
-# wrapper function to edit json file inplace
-#
-jqi() {
-  cat <<< "$(jq "$1" < "$2")" > "$2"
-}
-
-echo "Updating controller docker image configuration"
-jqi '.cortexApiProxy = "'${CORTEX_API_PROXY_IMAGE}'"' ${DIR}/../packages/controller-config/src/docker-images.json
-
-jqi '.lokiApiProxy = "'${LOKI_API_PROXY_IMAGE}'"' ${DIR}/../packages/controller-config/src/docker-images.json
-
-jqi '.ddApi = "'${DD_API_PROXY_IMAGE}'"' ${DIR}/../packages/controller-config/src/docker-images.json
-
-jqi '.configApi = "'${CONFIG_API_PROXY_IMAGE}'"' ${DIR}/../packages/controller-config/src/docker-images.json
-
-jqi '.app = "'${OPSTRACE_APP_IMAGE}'"' ${DIR}/../packages/controller-config/src/docker-images.json
-
-jqi '.graphqlEngine = "'${OPSTRACE_GRAPHQL_IMAGE}'"' ${DIR}/../packages/controller-config/src/docker-images.json
-
-echo "Updated docker images"
-git --no-pager diff ${DIR}/../packages/controller-config/src/docker-images.json
