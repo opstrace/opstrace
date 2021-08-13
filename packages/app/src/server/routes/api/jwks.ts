d5d7d0a733a96e3d6e1b7711e501672b042d48aa
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-08-13 11:28:40 +0200

    ui: app: jwks helper: handle non-2xx, add timestamps to errs
    For detecting https://github.com/auth0/node-jwks-rsa/issues/257
    it makes sense to have timestamps in error messages.
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- packages/app/src/server/routes/api/jwks.ts
+++ packages/app/src/server/routes/api/jwks.ts
@@ -191,7 +191,7 @@ export async function fetch(url: string) {
       return getPotantiallyStaleJWKS();
     }
     throw new Error(
-      "JWKS fetcher: HTTP GET failed and LAST_JWKS_POTENTIALLY_STALE not set"
+      `JWKS fetcher: HTTP GET failed and LAST_JWKS_POTENTIALLY_STALE not set (${Date.now()})`
     );
   }
 
@@ -199,6 +199,22 @@ export async function fetch(url: string) {
   // as last response, not yet explicitly treated by code although the
   // JSON deser handler will catch that
   // TODO if (resp.code !== 200) { ... getPotantiallyStaleJWKS() ... }
+
+  if (resp.statusCode !== 200) {
+    log.warning(
+      `JWKS fetcher: giving up HTTP GET, last response status code: ${resp.statusCode}`
+    );
+    if (LAST_JWKS_POTENTIALLY_STALE !== undefined) {
+      return getPotantiallyStaleJWKS();
+    }
+
+    // Note(JP): by adding `Date.now()` to this error message I found that the
+    // jwks-rsa cache would cache errors, too. Doh! This really undermines..
+    // see https://github.com/auth0/node-jwks-rsa/issues/257
+    throw new Error(
+      `JWKS fetcher: HTTP GET failed and LAST_JWKS_POTENTIALLY_STALE not set (${Date.now()})`
+    );
+  }
 
   let newKeySet: object | undefined;
   try {
@@ -216,6 +232,24 @@ export async function fetch(url: string) {
   }
 
   throw new Error(
-    "JWKS fetcher: request succeeded, deserialization failed, LAST_JWKS_POTENTIALLY_STALE not set"
+    `JWKS fetcher: request succeeded, deserialization failed, LAST_JWKS_POTENTIALLY_STALE not set (${Date.now()})`
   );
 }
+
+// const DUMMY_JWKS = {
+//   keys: [
+//     {
+//       kty: "RSA",
+//       kid: "cc34c0a0-bd5a-4a3c-a50d-a2a7db7643df",
+//       use: "sig",
+//       n: "pjdss8ZaDfEH6K6U7GeW2nxDqR4IP049fk1fK0lndimbMMVBdPv_hSpm8T8EtBDxrUdi1OHZfMhUixGaut-3nQ4GG9nM249oxhCtxqqNvEXrmQRGqczyLxuh-fKn9Fg--hS9UpazHpfVAFnB5aCfXoNhPuI8oByyFKMKaOVgHNqP5NBEqabiLftZD3W_lsFCPGuzr4Vp0YS7zS2hDYScC2oOMu4rGU1LcMZf39p3153Cq7bS2Xh6Y-vw5pwzFYZdjQxDn8x8BG3fJ6j8TGLXQsbKH1218_HcUJRvMwdpbUQG5nvA2GXVqLqdwp054Lzk9_B_f1lVrmOKuHjTNHq48w",
+//       e: "AQAB",
+//       d: "ksDmucdMJXkFGZxiomNHnroOZxe8AmDLDGO1vhs-POa5PZM7mtUPonxwjVmthmpbZzla-kg55OFfO7YcXhg-Hm2OWTKwm73_rLh3JavaHjvBqsVKuorX3V3RYkSro6HyYIzFJ1Ek7sLxbjDRcDOj4ievSX0oN9l-JZhaDYlPlci5uJsoqro_YrE0PRRWVhtGynd-_aWgQv1YzkfZuMD-hJtDi1Im2humOWxA4eZrFs9eG-whXcOvaSwO4sSGbS99ecQZHM2TcdXeAs1PvjVgQ_dKnZlGN3lTWoWfQP55Z7Tgt8Nf1q4ZAKd-NlMe-7iqCFfsnFwXjSiaOa2CRGZn-Q",
+//       p: "4A5nU4ahEww7B65yuzmGeCUUi8ikWzv1C81pSyUKvKzu8CX41hp9J6oRaLGesKImYiuVQK47FhZ--wwfpRwHvSxtNU9qXb8ewo-BvadyO1eVrIk4tNV543QlSe7pQAoJGkxCia5rfznAE3InKF4JvIlchyqs0RQ8wx7lULqwnn0",
+//       q: "ven83GM6SfrmO-TBHbjTk6JhP_3CMsIvmSdo4KrbQNvp4vHO3w1_0zJ3URkmkYGhz2tgPlfd7v1l2I6QkIh4Bumdj6FyFZEBpxjE4MpfdNVcNINvVj87cLyTRmIcaGxmfylY7QErP8GFA-k4UoH_eQmGKGK44TRzYj5hZYGWIC8",
+//       dp: "lmmU_AG5SGxBhJqb8wxfNXDPJjf__i92BgJT2Vp4pskBbr5PGoyV0HbfUQVMnw977RONEurkR6O6gxZUeCclGt4kQlGZ-m0_XSWx13v9t9DIbheAtgVJ2mQyVDvK4m7aRYlEceFh0PsX8vYDS5o1txgPwb3oXkPTtrmbAGMUBpE",
+//       dq: "mxRTU3QDyR2EnCv0Nl0TCF90oliJGAHR9HJmBe__EjuCBbwHfcT8OG3hWOv8vpzokQPRl5cQt3NckzX3fs6xlJN4Ai2Hh2zduKFVQ2p-AF2p6Yfahscjtq-GY9cB85NxLy2IXCC0PF--Sq9LOrTE9QV988SJy_yUrAjcZ5MmECk",
+//       qi: "ldHXIrEmMZVaNwGzDF9WG8sHj2mOZmQpw9yrjLK9hAsmsNr5LTyqWAqJIYZSwPTYWhY4nu2O0EY9G9uYiqewXfCKw_UngrJt8Xwfq1Zruz0YY869zPN4GiE9-9rzdZB33RBw8kIOquY3MK74FMwCihYx_LiU2YTHkaoJ3ncvtvg"
+//     }
+//   ]
+// };
