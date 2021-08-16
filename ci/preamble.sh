869d55eec3184dfdca31ffe081b76df2d03d22b4
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-08-16 21:09:55 +0200

    ci: preamble: do not remove looker package.json
    needed in the looker build steps below,
    not done in bg w/ unique build dir
    
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- ci/preamble.sh
+++ ci/preamble.sh
@@ -134,7 +134,7 @@ echo "--- start in background: yarn --frozen-lockfile"
 # container image is not rebuilt).
 mv packages/app/package.json packages/app/package.json.deactivated
 mv test/test-remote/package.json test/test-remote/package.json.deactivated
-mv test/test-remote/looker/package.json test/test-remote/looker/package.json.deactivated
+#mv test/test-remote/looker/package.json test/test-remote/looker/package.json.deactivated
 mv test/browser/package.json test/browser/package.json.deactivated
 mv packages/controller/package.json packages/controller/package.json.deactivated
 
