1b16cc99cdaad73b8b7b15a61b10ad78d6dbe55f
Dr. Jan-Philip Gehrcke <jp@opstrace.com>
2021-08-21 12:50:13 +0200

    test-remote: adjust to rename
    Signed-off-by: Dr. Jan-Philip Gehrcke <jp@opstrace.com>

--- test/test-remote/test_loki_log_ingest_api.ts
+++ test/test-remote/test_loki_log_ingest_api.ts
@@ -572,7 +572,7 @@ suite("Loki API test suite", function () {
 
   test("short dummystream insert, validate via query", async function () {
     const stream = new LogSeries({
-      n_entries_per_stream_fragment: 10 ** 2,
+      n_samples_per_series_fragment: 10 ** 2,
       n_chars_per_msg: 90,
       starttime: ZonedDateTime.now(),
       //starttime: ZonedDateTime.parse("2020-02-20T17:40:40.000000000Z"),
@@ -609,7 +609,7 @@ suite("Loki API test suite", function () {
       const streamname = `${nameprefix}-${i.toString().padStart(4, "0")}`;
 
       const stream = new LogSeries({
-        n_entries_per_stream_fragment: 4 * 10 ** 3,
+        n_samples_per_series_fragment: 4 * 10 ** 3,
         n_chars_per_msg: 90,
         starttime: ZonedDateTime.now(),
         uniqueName: streamname,
@@ -647,7 +647,7 @@ suite("Loki API test suite", function () {
     const NentriesSent =
       N_concurrent_streams *
       nFragmentsPerStream *
-      streams[0].n_entries_per_stream_fragment;
+      streams[0].n_samples_per_series_fragment;
     const NcharsSent = NentriesSent * streams[0].n_chars_per_msg;
     const MegacharsSent = NcharsSent / 10 ** 6; // int division ok?
     const MegacharsPerSec = MegacharsSent / durationSeconds;
