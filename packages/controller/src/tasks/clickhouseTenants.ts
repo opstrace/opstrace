d2e892ac29584ab764186c4e8f0117ce0a02994c
Nick Parker <nick@opstrace.com>
2021-11-04 16:05:15 +1300

    controller: add 'just in case' regrant of permissions after controller restart
    Signed-off-by: Nick Parker <nick@opstrace.com>

--- packages/controller/src/tasks/clickhouseTenants.ts
+++ packages/controller/src/tasks/clickhouseTenants.ts
@@ -41,14 +41,14 @@ export function* clickhouseTenantsReconciler(): Generator<
       return;
     }
     const dbClient2 = dbClient;
+    var initializedGrants = false;
 
     while (true) {
       // loop through again in 5s
       yield delay(5 * SECOND);
 
       const state: State = yield select();
-      const tenants = state.tenants.list.tenants;
-      if (tenants.length === 0) {
+      if (state.tenants.list.tenants.length === 0) {
         // tenants not loaded? something's not right. wait and try again after delay
         // In particular we don't want to drop all DBs if the tenants list is missing/empty
         log.warning("skipping ClickHouse tenant sync: empty tenants list");
@@ -72,31 +72,39 @@ export function* clickhouseTenantsReconciler(): Generator<
 
       // Convert e.g. "system" => "tenant_system", "user-tenant" => "tenant_user_tenant"
       // Clickhouse does not like dashes, so we map them to underscores which are supported
-      const clickhouseTenants = tenants.map(tenant =>
+      const clickhouseTenants = state.tenants.list.tenants.map(tenant =>
         getTenantClickHouseName(tenant)
       );
 
       const currentDBs = state.clickhouse.Databases.resources;
       const currentUsers = state.clickhouse.Users.resources;
 
+      // Once per controller process, try to initialize grants for all DB tenant users.
+      // This is to allow automatically "repairing" permissions of existing tenant users,
+      // just in case the controller had just restarted at a very unlucky time when creating them.
+      // This is ultimately just a best-effort attempt at repairing grants if they are missing,
+      // and should be a no-op most of the time. This repair can be removed if we start checking
+      // SHOW GRANTS, as mentioned below.
+      var permissionsToGrant: string[] = [];
+      if (!initializedGrants) {
+        permissionsToGrant = clickhouseTenants.map(queryGrantUser);
+        initializedGrants = true;
+      }
+
       const dbsToAdd = clickhouseTenants
         .filter(clickhouseTenant => !currentDBs.includes(clickhouseTenant))
-        .map(
-          dbToAdd => `CREATE DATABASE IF NOT EXISTS ${dbToAdd} ENGINE=Atomic`
-        );
+        .map(queryCreateDatabase);
       const usersToAdd = clickhouseTenants
         .filter(clickhouseTenant => !currentUsers.includes(clickhouseTenant))
         .flatMap(userToAdd =>
           // There is a potential bug here where the controller could exit between the
           // CREATE USER and GRANT queries, resulting in a degraded user missing permissions.
           // To fix this properly we could poll permissions using SHOW GRANTS, but parsing that would be
-          // complicated. For now we punt on this and just ensure the queries are run sequentially (below).
+          // complicated. For now we somewhat punt on this, reapplying grants after a restart (above)
+          // and ensuring that queries are run sequentially (below).
           // Note that the CREATE USER + GRANT unfortunately cannot be sent as a single semicolon-separated
           // query/transaction: the server returns a 'Multi-statements are not allowed' error.
-          [
-            `CREATE USER IF NOT EXISTS ${userToAdd} IDENTIFIED WITH plaintext_password BY '${userToAdd}_password' HOST ANY DEFAULT DATABASE ${userToAdd};`,
-            `GRANT SELECT, INSERT, ALTER, CREATE, DROP, TRUNCATE, OPTIMIZE, SHOW ON ${userToAdd}.* TO ${userToAdd}`
-          ]
+          [queryCreateUser(userToAdd), queryGrantUser(userToAdd)]
         );
 
       // Search for users and DBs that start with 'tenant_',
@@ -108,17 +116,18 @@ export function* clickhouseTenantsReconciler(): Generator<
             curuser.startsWith(tenantPrefix) &&
             !clickhouseTenants.includes(curuser)
         )
-        .map(tenantUserToRemove => `DROP USER IF EXISTS ${tenantUserToRemove}`);
+        .map(queryDropUser);
       const dbsToRemove = currentDBs
         .filter(
           curdb =>
             curdb.startsWith(tenantPrefix) && !clickhouseTenants.includes(curdb)
         )
-        .map(tenantDbToRemove => `DROP DATABASE IF EXISTS ${tenantDbToRemove}`);
+        .map(queryDropDatabase);
 
       // The ordering here probably doesn't matter,
       // but it feels like DBs should only be created before (and deleted after) their associated users.
-      const queries = dbsToAdd
+      const queries = permissionsToGrant
+        .concat(dbsToAdd)
         .concat(usersToAdd)
         .concat(usersToRemove)
         .concat(dbsToRemove);
@@ -142,4 +151,24 @@ export function* clickhouseTenantsReconciler(): Generator<
       }
     }
   });
+}
+
+function queryCreateUser(userToCreate: string): string {
+  return `CREATE USER IF NOT EXISTS ${userToCreate} IDENTIFIED WITH plaintext_password BY '${userToCreate}_password' HOST ANY DEFAULT DATABASE ${userToCreate}`;
+}
+
+function queryGrantUser(userToGrant: string): string {
+  return `GRANT SELECT, INSERT, ALTER, CREATE, DROP, TRUNCATE, OPTIMIZE, SHOW ON ${userToGrant}.* TO ${userToGrant}`;
+}
+
+function queryDropUser(userToDrop: string): string {
+  return `DROP USER IF EXISTS ${userToDrop}`;
+}
+
+function queryCreateDatabase(dbToCreate: string): string {
+  return `CREATE DATABASE IF NOT EXISTS ${dbToCreate} ENGINE=Atomic`;
+}
+
+function queryDropDatabase(dbToDrop: string): string {
+  return `DROP DATABASE IF EXISTS ${dbToDrop}`;
 }
